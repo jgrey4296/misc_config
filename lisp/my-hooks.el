3753a9d331e52e91de8a92149e620bb1f8a6bc27
John Grey <jgrey@ucsc.edu>
2017-03-28 12:02:03 +0100

    added a hook to open org links of images correctly

--- lisp/my-hooks.el
+++ lisp/my-hooks.el
@@ -1,3 +1,4 @@
+
 ;;context highlighting
 (add-hook 'js2-mode-hook #'context-coloring-mode)
 
@@ -7,7 +8,6 @@
 ;; eval-expression:
 (add-hook 'eval-expression-minibuffer-setup-hook #'context-coloring-mode) ; 24.4+
 ;;(add-hook 'minibuffer-setup-hook #'context-coloring-mode)                 ; 24.3
-
 
 ;;Add Hooks to enable spell checking for text and latex
 ;;Requires flyspell and ispell to be installed
@@ -24,6 +24,10 @@
 (dolist (hook '(org-mode-hook))
   (add-hook hook (lambda () (flyspell-mode 1)))
   (add-hook hook (lambda () (writegood-mode 1)))
+  ;;Add some file types to org-file-apps
+  (add-hook hook (lambda () (add-to-list 'org-file-apps
+					 '("\\.JP[E]?G\\|GIF\\|PNG" . "open %s")
+					 'APPEND)))
   )
 
 (dolist (hook '(markdown-mode))
@@ -114,21 +118,21 @@
                )))
 
 ;; Use human readable Size column instead of original one
-(define-ibuffer-column size-h
-  (:name "Size" :inline t)
-  (cond
-   ((> (buffer-size) 1000000) (format "%7.1fM" (/ (buffer-size) 1000000.0)))
-   ((> (buffer-size) 100000) (format "%7.0fk" (/ (buffer-size) 1000.0)))
-   ((> (buffer-size) 1000) (format "%7.1fk" (/ (buffer-size) 1000.0)))
-   (t (format "%8db" (buffer-size)))))
+;; (define-ibuffer-column size-h
+;;   (:name "Size" :inline t)
+;;   (cond
+;;    ((> (buffer-size) 1000000) (format "%7.1fM" (/ (buffer-size) 1000000.0)))
+;;    ((> (buffer-size) 100000) (format "%7.0fk" (/ (buffer-size) 1000.0)))
+;;    ((> (buffer-size) 1000) (format "%7.1fk" (/ (buffer-size) 1000.0)))
+;;    (t (format "%8db" (buffer-size)))))
 
-;; Modify the default ibuffer-formats
-  (setq ibuffer-formats
-	'((mark modified read-only " "
-		(name 18 18 :left :elide)
-		" "
-		(size-h 9 -1 :right)
-		" "
-		(mode 16 16 :left :elide)
-		" "
-		filename-and-process)))
+;; ;; Modify the default ibuffer-formats
+;; (setq ibuffer-formats
+;;       '((mark modified read-only " "
+;;          (name 18 18 :left :elide)
+;;          " "
+;;          (size-h 9 -1 :right)
+;;          " "
+;;          (mode 16 16 :left :elide)
+;;          " "
+;;          filename-and-process)))

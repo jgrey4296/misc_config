16bb9025196f6df0e9d0d4cdc9868eee9713cabf
John Grey <jgrey@ucsc.edu>
2017-05-16 17:06:05 +0100

    added tidal setup code

--- lisp/my-emacs.el
+++ lisp/my-emacs.el
@@ -4,6 +4,12 @@
 ;(setq debug-on-error 1)
 
 ;; Load paths
+(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
+(package-initialize)
+
+(setq load-path (cons "~/.emacs.setup/lisp-lib/" load-path))
+(require 'tidal)
+(setq tidal-interpreter "/usr/local/bin/ghci")
 
 (let ((default-directory (expand-file-name "~/.emacs.setup/")))
   (normal-top-level-add-subdirs-to-load-path))

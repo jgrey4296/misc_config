c7e787f891e6566450ed9e219a1efb1e50e80822
John Grey <jgrey@ucsc.edu>
2018-11-24 06:41:42 +0000

    added erlang to the load path

--- lisp/my-emacs.el
+++ lisp/my-emacs.el
@@ -8,8 +8,8 @@
 (package-initialize)
 
 (setq load-path (cons "~/.emacs.setup/lisp-lib/" load-path))
-(require 'tidal)
-(setq tidal-interpreter "/usr/local/bin/ghci")
+
+
 
 (let ((default-directory (expand-file-name "~/.emacs.setup/")))
   (normal-top-level-add-subdirs-to-load-path))
@@ -18,6 +18,9 @@
 (add-to-list 'load-path (expand-file-name "~/.emacs.setup/lisp-lib"))
 (add-to-list 'load-path "/Users/jgrey/github/otherlibs/python-django.el")
 (add-to-list 'load-path "/Users/jgrey/github/otherlibs/pony-mode/src")
+
+;;load path for erlang tools
+(add-to-list 'load-path "/usr/local/opt/erlang/lib/erlang/lib/tools-3.0.1/emacs")
 
 (load "my-functions")
 (load "my-aliases")

e1887efb4aca65b4ee4f4e7dac458f2a95e60eb8
John Grey <jgrey@ucsc.edu>
2018-11-24 06:42:04 +0000

    formatted the modes file better, set up tidal and erlang too

--- lisp/my-modes.el
+++ lisp/my-modes.el
@@ -9,6 +9,16 @@
 (icy-mode 1)
 (outline-minor-mode)
 (org-mode)
+(global-auto-revert-mode 1)  ;auto revert buffers when changed on disk
+(show-paren-mode t)          ;visualize()
+(blink-cursor-mode -1)       ;no cursor blinking
+(tool-bar-mode -1)           ;disable the awful toolbar
+(menu-bar-mode -1)           ;no menu, you can toggle it with C-c m
+;(iswitchb-mode t)           ;use advanced tab switching
+;(scroll-bar-mode -1)        ;disable the sroll bar
+(pinentry-start)             ;starts pinentry for gpg use in terminal
+
+
 (eval-after-load 'auto-complete '(global-auto-complete-mode 1))
 (eval-after-load 'flycheck '(global-flycheck-mode 1))
 
@@ -18,29 +28,25 @@
 (require 'netlogo-mode)
 (require 'sclang)
 (require 'trie-mode)
+(require 'tidal)
+
+;;set the tidal interpreter
+(setq tidal-interpreter "/usr/local/bin/ghci")
+
+;;setting up erlang
+;; (also has a load path set in root el file)
+(setq erlang-root-dir "/usr/local/opt/erlang")
+(setq exec-path (cons "usr/local/opt/erlang/bin" exec-path))
+(require 'erlang-start)
+
+
 
 (autoload 'drools-mode "drools-mode")
 ;no longer needed with outline and occur:
 ;(autoload "folding.el") ;'nomessage 'noerror)
 
 
-
 ;(autoload 'csharp-mode "csharp-mode" "Major mode for editing C# code." t)
 ;(setq auto-mode-alist
 ;      (append '(("\\.cs$" . csharp-mode)) auto-mode-alist))
 
-
-
-
-
-(global-auto-revert-mode 1)  ;auto revert buffers when changed on disk
-(show-paren-mode t)          ;visualize()
-;(iswitchb-mode t)            ;use advanced tab switching
-(blink-cursor-mode -1)       ;no cursor blinking
-(tool-bar-mode -1)           ;disable the awful toolbar
-(menu-bar-mode -1)           ;no menu, you can toggle it with C-c m
-;(scroll-bar-mode -1)         ;disable the sroll bar
-
-
-
-;org mode stuff:

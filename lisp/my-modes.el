536ab8be6930a2f6ddd4f71a9df0f0ee2be35f3a
John Grey <jgrey@ucsc.edu>
2018-12-11 00:05:06 +0000

    cleanup of modes

--- lisp/my-modes.el
+++ lisp/my-modes.el
@@ -6,44 +6,29 @@
 (global-linum-mode 1)
 (column-number-mode 1)
 (yas-global-mode 1)
-(icy-mode 1)
 (outline-minor-mode)
-(org-mode)
+(org-mode)                   ;; Start org first, then icy, or weird things happen
+(icy-mode 1)
 (global-auto-revert-mode 1)  ;auto revert buffers when changed on disk
 (show-paren-mode t)          ;visualize()
 (blink-cursor-mode -1)       ;no cursor blinking
 (tool-bar-mode -1)           ;disable the awful toolbar
 (menu-bar-mode -1)           ;no menu, you can toggle it with C-c m
-;(iswitchb-mode t)           ;use advanced tab switching
-;(scroll-bar-mode -1)        ;disable the sroll bar
 (pinentry-start)             ;starts pinentry for gpg use in terminal
 
-
-(eval-after-load 'auto-complete '(global-auto-complete-mode 1))
-(eval-after-load 'flycheck '(global-flycheck-mode 1))
-
+(autoload 'netlogo-mode "netlogo-mode" :interactive t)
+(autoload 'drools-mode "drools-mode" :interactive t)
 (require 'chuck-mode)
-;;(require 'python-django)
-;;(require 'pony-mode)
-(require 'netlogo-mode)
 (require 'sclang)
 (require 'trie-mode)
 (require 'tidal)
-
-;;set the tidal interpreter
-(setq tidal-interpreter "/usr/local/bin/ghci")
-(setq tidal-interpreter-arguments
-      (list "-ghci-script" (expand-file-name "~/github/languageLearning/tidal/.ghci")))
-            
-
-;;setting up erlang
-;; (also has a load path set in root el file)
-(setq erlang-root-dir "/usr/local/opt/erlang")
-(setq exec-path (cons "usr/local/opt/erlang/bin" exec-path))
 (require 'erlang-start)
+;;(require 'dropdown-list)
+;;(require 'python-django)
+;;(require 'pony-mode)
+;;(iswitchb-mode t)           ;use advanced tab switching
+;;(scroll-bar-mode -1)        ;disable the sroll bar
 
-(autoload 'drools-mode "drools-mode")
-;(autoload 'csharp-mode "csharp-mode" "Major mode for editing C# code." t)
-;(setq auto-mode-alist
-;      (append '(("\\.cs$" . csharp-mode)) auto-mode-alist))
+(eval-after-load 'auto-complete '(global-auto-complete-mode 1))
+(eval-after-load 'flycheck '(global-flycheck-mode 1))
 

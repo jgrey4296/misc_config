b42fed3250a14ac63c9e06f3bec8f553c57cfeff
John Grey <jgrey@ucsc.edu>
2018-12-11 00:06:08 +0000

    refactored variable defintion

--- lisp/my-variables.el
+++ lisp/my-variables.el
@@ -1,32 +1,66 @@
-;Variable Setting:
-(setq indent-tabs-mode nil)
-(setq backup-directory-alist `(("." . "~/.emacs.d/backups/")))
-(setq transient-mark-mode t)
-(setq default-directory "~/")
-(setq default-auto-fill-function `do-auto-fill)
-(setq inhibit-startup-screen 1)
+;; Variable Setting:
+(setq-default indent-tabs-mode nil
+              backup-directory-alist `(("." .  "~/.emacs.d/backups/"))
+              transient-mark-mode t
+              default-directory (expand-file-name "~/")
+              default-auto-fill-function `do-auto-fill
+              inhibit-startup-screen 1
+              major-mode `org-mode
+              ;;Variables for sclang
+              sclang-library-configuration-file (expand-file-name "~/.emacs.setup/setup_files/sclang.yaml")
+              sclang-udp-port 57120
+              ;;set the tidal interpreter
+              tidal-interpreter "/usr/local/bin/ghci"
+              tidal-interpreter-arguments
+              (list "-ghci-script" (expand-file-name "~/github/languageLearning/tidal/.ghci"))
+              ;;setting up erlang
+              ;; (also has a load path set in root el file)
+              erlang-root-dir "/usr/local/opt/erlang"
+              exec-path (cons "/usr/local/opt/erlang/bin" exec-path)
+              ;;dropdown list use for yas
+              yas-prompt-functions '(yas-dropdown-prompt
+                                     yas-ido-prompt
+                                     yas-completing-prompt)
+              yas-snippet-dirs `( ,(expand-file-name "~/.emacs.setup/snippets/")
+                                 ,(expand-file-name "~/github/otherLibs/yasnippet-snippets/snippets")
+                                 ,(expand-file-name "~/github/otherLibs/yasnippet-snippets"))
+              ;;personal iSpell dictionary
+              ispell-personal-dictionary (expand-file-name "~/.emacs.setup/setup_files/.ispell_english")
+              ;;Custom Scratch Buffer
+              scratchBufferFile (expand-file-name "~/.emacs.setup/setup_files/scratch")
+              initial-scratch-message (file-string scratchBufferFile)
+              ;;flyspell-issue-message-flag nil)
+              ;;IBuffer:
+              ibuffer-never-show-predicates `("\\.ds_store")
+              ;;dired:
+              completion-ignored-extensions (cons ".DS_Store" completion-ignored-extensions)
+              dired-omit-files "^\\.?#\\|^\\.$\\|^\\.DS_Store$\\|^\\.git$\\|^__pycache__$"
+              ;;dired-omit-extensions (cons ".DS_Store" dired-omit-extensions)
+              ;;shell setup:
+              ;;explicit-shell-file-name "/opt/local/bin/bash"
+              shell-file-name "bash"
+              ;;explicit-bash-args ""
+              ;;use spaces instead of tabs
+              tab-width 4
+              indent-tabs-mode nil 
+              ;;"tab" with in c-related modes
+              c-basic-offset 4     
+              ;;delete more than one space
+              c-hungry-delete-key t 
+              ;;disable the annoying doc checker
+              flycheck-disabled-checkers '(emacs-lisp-checkdoc)
+              flycheck-indication-mode 'right-fringe
+              ;SCLANG Variables:
+              sclang-auto-scroll-post-buffer t
+              sclang-eval-line-forward nil
+              sclang-help-path (quote ("/Applications/SuperCollider/Help"))
+              sclang-runtime-directory (expand-file-name "~/.sclang/")
+              auto-save-file-name-transforms '((".*" "~/.emacs.d/autosaves/" t))
+              ;;abbrev-file complaint quieting
+              abbrev-file-name "~/.emacs.d/abbrevs_defs"
+)              
 
-(setq-default major-mode `org-mode)
-
-;;Variables for sclang
-(setq sclang-library-configuration-file "~/.emacs.setup/setup_files/sclang.yaml")
-(setq sclang-udp-port 57120)
-
-
-
-;dropdown list use for yas
-;;(require 'dropdown-list)
-;;(setq yas-prompt-functions '(yas-dropdown-prompt
-;;                             yas-ido-prompt
-;;                             yas-completing-prompt))
-
-
-(setq yas-snippet-dirs '("~/.emacs.setup/snippets/"
-                         "/Users/johngrey/github/otherLibs/yasnippet-snippets/snippets"
-                         "/Users/jgrey/github/otherLibs/yasnippet-snippets"))
-
-
-
+(setenv "SHELL" shell-file-name)
 ;adding melpa to package archives:
 (when (> emacs-major-version 23)
   (require 'package)
@@ -35,66 +69,17 @@
 	       '("melpa" . "http://melpa.milkbox.net/packages/")
 	       'APPEND))
 
-;file association:
-(setq auto-mode-alist (cons '(".*\.pde" . java-mode) auto-mode-alist))
-(setq auto-mode-alist (cons '(".*\.pl" .  prolog-mode) auto-mode-alist))
-(setq auto-mode-alist (cons '(".*\.py" .  python-mode) auto-mode-alist))
-(setq auto-mode-alist (cons '(".*\.emacs" . lisp-mode) auto-mode-alist))
-(setq auto-mode-alist (cons '(".*\.js" . js-mode) auto-mode-alist))
-(setq auto-mode-alist (cons '(".*\.md" . markdown-mode) auto-mode-alist))
-
-;;(defvar scratchBufferFile (expand-file-name "./scratch.txt"))
-;;(setq initial-scratch-message (file-string scratchBufferFile))
-(setq flyspell-issue-message-flag nil)
-
 ;For setting sbcl lisp instead
 ;;;(setq inferior-lisp-program "/usr/local/bin/sbcl")
-
-;Ibuffer filter groups:
-;need to rework this:
-;; (setq ibuffer-saved-filter-groups
-;;       '(("home"
-;; 	  ("emacs" (filename . ".emacs"))
-;; 	  ("JG" (filename . "\*JG\*"))
-;; 	 ("Headers" (filename . "\.h$"))
-;; 	 ("Code" (or (mode . c-mode) 
-;; 		     (mode . c++-mode)
-;; 		     (mode . java-mode)
-;; 		     (mode . javascript-mode)
-;; 		     (filename . "\.js$"))
-;; 	  )
-;; 	  ("Web" (or (mode . html-mode)
-;; 			 (mode . css-mode)))
-;; 	      ("Readme" (or (name . "\*README\*")
-;; 			      (name . "\*Help\*")
-;; 			       (name . "\*Apropos\*")
-;; 			            (name . "\*info\*"))))))
 
 (setq ibuffer-formats '((mark modified read-only " " (name 15 18 :left
 			      :elide) " " (size 5 5 :right :elide) " "
 			      mode)))
 
-(make-directory "~/.emacs.d/autosaves/" t)
-(make-directory "~/.emacs.d/backups/" t)
-
-;Custom Variables:
-(custom-set-variables
- '(sclang-auto-scroll-post-buffer t)
- '(sclang-eval-line-forward nil)
- '(sclang-help-path (quote ("/Applications/SuperCollider/Help")))
- '(sclang-runtime-directory "~/.sclang/")
- '(auto-save-file-name-transforms '((".*" "~/.emacs.d/autosaves/`" t))) 
- '(backup-directory-alist '((".*" . "~/.emacs.d/backups/")))
- )
+(make-directory (expand-file-name "~/.emacs.d/autosaves/") t)
+(make-directory (expand-file-name "~/.emacs.d/backups/") t)
 
 
-;;(custom-set-variables '(haskel-mode-hook '(turn-on-haskell-indentation)))
-
-;shell setup:
-;;(setq explicit-shell-file-name "/opt/local/bin/bash")
-(setq shell-file-name "bash")
-;(setq explicit-bash-args "")
-(setenv "SHELL" shell-file-name)
 
 ;Sweeping blow to use utf8
 ;http://steckerhalter.co.vu/steckemacs.html#sec-1
@@ -102,14 +87,4 @@
 (set-keyboard-coding-system 'utf-8)
 (set-language-environment "UTF-8")
 (prefer-coding-system 'utf-8)
-
-(setq-default
- tab-width 4
- indent-tabs-mode nil                   ;use spaces instead of tabs
- c-basic-offset 4                       ;"tab" with in c-related modes
- c-hungry-delete-key t                  ;delete more than one space
- )
-
-(setq-default flycheck-disabled-checkers '(emacs-lisp-checkdoc)) ;disable the annoying doc checker
-(setq flycheck-indication-mode 'right-fringe)
 

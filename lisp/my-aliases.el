46ea0b2ee852ebb85c9f556e16f1011269f6d7cd
John Grey <jgrey@ucsc.edu>
2018-12-17 22:07:19 +0000

    deleted obsolete files

--- lisp/my-aliases.el
+++
@@ -1,7 +0,0 @@
-;aliases:
-(defalias 'list-buffers 'ibuffer)
-(defalias 'db 'display-all-buffers)
-(defalias 'ded 'toggle-window-dedicated)
-(defalias 'lorem 'Lorem-ipsum-insert-paragraphs)
-(defalias 'cr 'comment-region)
-(defalias 'scr 'scratch)

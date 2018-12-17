c19d970450850bf72772e1955d587056e87e665a
John Grey <jgrey@ucsc.edu>
2018-12-17 22:27:02 +0000

    cleaning up init.el to use jg_layer

--- init.el
+++ init.el
@@ -30,23 +30,17 @@ values."
    ;; If non-nil layers with lazy install support are lazy installed.
    ;; List of additional paths where to look for configuration layers.
    ;; Paths must have a trailing slash (i.e. `~/.mycontribs/')
-   dotspacemacs-configuration-layer-path '()
+   dotspacemacs-configuration-layer-path '("~/.spacemacs.d/layers/") 
    ;; List of configuration layers to load.
    dotspacemacs-configuration-layers
    '(
-     helm
-     auto-completion
-     better-defaults
-     emacs-lisp
-     git
-     markdown
-     org
+     ;;------------------------------------------------------------
+     ;;MY LAYER: 
+     jg_layer
+     ;;-----------------------------------------------------------
      (shell :variables
             shell-default-height 30
             shell-default-position 'bottom)
-     spell-checking
-     syntax-checking
-     version-control
      )
    ;; List of additional packages that will be installed without being
    ;; wrapped in a layer. If you need some configuration for these
@@ -307,10 +301,13 @@ executes.
  This function is mostly useful for variables that need to be set
 before packages are loaded. If you are unsure, you should try in setting them in
 `dotspacemacs/user-config' first."
+  (add-to-list 'load-path (expand-file-name "~/.spacemacs.d/lisp"))
+  (add-to-list 'load-path (expand-file-name "~/.spacemacs.d/lisp-lib"))
+  (add-to-list 'load-path (expand-file-name "~/github/otherlibs/python-django.el"))
+  (add-to-list 'load-path (expand-file-name "~/github/otherlibs/pony-mode/src"))
+  (add-to-list 'load-path "/usr/local/opt/erlang/lib/erlang/lib/tools-3.0.1/emacs")
   )
 
-(defun silence ()
-  (interactive))
 
 ;;------------------------------------------------------------------------------
 ;;     USER CONFIG
@@ -322,13 +319,7 @@ layers configuration.
 This is the place where most of your configurations should be done. Unless it is
 explicitly specified that a variable should be set before a package is loaded,
 you should place your code here."
-  ;;(add-hook 'after-change-major-mode-hook 'fci-mode)
-  (add-hook 'change-major-mode-after-body-hook 'fci-mode)
-  (neotree-show)
-  ;; dummy silence definition
-  (xterm-mouse-mode 0) 
-  (setq-default dotspacemacs-configuration-layer-path '("~/.spacemacs.d/layers/"))
-  (setq-default dotspacemacs-line-numbers t)
+  
   )
 ;;------------------------------------------------------------------------------
 ;;------------------------------------------------------------------------------
@@ -341,12 +332,44 @@ you should place your code here."
  ;; If there is more than one, they won't work right.
  '(evil-escape-key-sequence "hg")
  '(evil-escape-mode t)
+ '(helm-always-two-windows t)
+ '(helm-autoresize-mode t)
+ '(helm-bookmark-show-location t t)
+ '(helm-descbinds-mode t)
+ '(helm-descbinds-window-style (quote split))
+ '(helm-display-function (quote spacemacs//display-helm-window))
+ '(helm-display-header-line nil)
+ '(helm-echo-input-in-header-line t)
+ '(helm-flx-for-helm-find-files nil)
+ '(helm-flx-mode t)
+ '(helm-fuzzy-matching-highlight-fn (quote helm-flx-fuzzy-highlight-match))
+ '(helm-fuzzy-sort-fn (quote helm-flx-fuzzy-matching-sort))
+ '(helm-locate-command "locate %s %s")
+ '(helm-locate-fuzzy-match 0)
+ '(helm-mode t)
+ '(helm-split-window-inside-p t)
+ '(mode-line-in-non-selected-windows t)
  '(package-selected-packages
    (quote
-    (xterm-color unfill smeargle shell-pop orgit org-projectile org-category-capture org-present org-pomodoro alert log4e gntp org-mime org-download mwim multi-term magit-gitflow htmlize helm-gitignore helm-company helm-c-yasnippet gnuplot gitignore-mode gitconfig-mode gitattributes-mode git-timemachine git-messenger git-link git-gutter-fringe+ git-gutter-fringe fringe-helper git-gutter+ git-gutter fuzzy flyspell-correct-helm flyspell-correct flycheck-pos-tip pos-tip flycheck evil-magit magit magit-popup git-commit ghub treepy graphql with-editor eshell-z eshell-prompt-extras esh-help diff-hl company-statistics company auto-yasnippet yasnippet auto-dictionary ac-ispell auto-complete mmm-mode markdown-toc markdown-mode gh-md ws-butler winum which-key volatile-highlights vi-tilde-fringe uuidgen use-package toc-org spaceline powerline restart-emacs request rainbow-delimiters popwin persp-mode pcre2el paradox spinner org-plus-contrib org-bullets open-junk-file neotree move-text macrostep lorem-ipsum linum-relative link-hint indent-guide hydra hungry-delete hl-todo highlight-parentheses highlight-numbers parent-mode highlight-indentation helm-themes helm-swoop helm-projectile helm-mode-manager helm-make projectile pkg-info epl helm-flx helm-descbinds helm-ag google-translate golden-ratio flx-ido flx fill-column-indicator fancy-battery eyebrowse expand-region exec-path-from-shell evil-visualstar evil-visual-mark-mode evil-unimpaired evil-tutor evil-surround evil-search-highlight-persist evil-numbers evil-nerd-commenter evil-mc evil-matchit evil-lisp-state smartparens evil-indent-plus evil-iedit-state iedit evil-exchange evil-escape evil-ediff evil-args evil-anzu anzu evil goto-chg undo-tree eval-sexp-fu highlight elisp-slime-nav dumb-jump f dash s diminish define-word column-enforce-mode clean-aindent-mode bind-map bind-key auto-highlight-symbol auto-compile packed aggressive-indent adaptive-wrap ace-window ace-link ace-jump-helm-line helm avy helm-core popup async))))
+    (yapfify yaml-mode web-mode web-beautify toml-mode tagedit slim-mode skewer-mode scss-mode sass-mode faceup racer pyvenv pytest pyenv-mode py-isort pug-mode pip-requirements tablist omnisharp lua-mode simple-httpd live-py-mode json-snatcher json-reformat multiple-cursors js2-mode js-doc intero ibuffer-projectile dash-functional hlint-refactor hindent helm-pydoc helm-hoogle helm-css-scss parsebib haskell-snippets haml-mode glsl-mode ghc geiser company-quickhelp flycheck-rust flycheck-haskell flycheck-dmd-dub emmet-mode disaster d-mode cython-mode csharp-mode web-completion-data tern company-ghci haskell-mode company-cabal company-c-headers company-auctex anaconda-mode coffee-mode cmm-mode cmake-mode clang-format rust-mode biblio-core auctex-latexmk auctex pythonic erlang xterm-color unfill smeargle shell-pop orgit org-projectile org-category-capture org-present org-pomodoro alert log4e gntp org-mime org-download mwim multi-term magit-gitflow htmlize helm-gitignore helm-company helm-c-yasnippet gnuplot gitignore-mode gitconfig-mode gitattributes-mode git-timemachine git-messenger git-link git-gutter-fringe+ git-gutter-fringe fringe-helper git-gutter+ git-gutter fuzzy flyspell-correct-helm flyspell-correct flycheck-pos-tip pos-tip flycheck evil-magit magit magit-popup git-commit ghub treepy graphql with-editor eshell-z eshell-prompt-extras esh-help diff-hl company-statistics company auto-yasnippet yasnippet auto-dictionary ac-ispell auto-complete mmm-mode markdown-toc markdown-mode gh-md ws-butler winum which-key volatile-highlights vi-tilde-fringe uuidgen use-package toc-org spaceline powerline restart-emacs request rainbow-delimiters popwin persp-mode pcre2el paradox spinner org-plus-contrib org-bullets open-junk-file neotree move-text macrostep lorem-ipsum linum-relative link-hint indent-guide hydra hungry-delete hl-todo highlight-parentheses highlight-numbers parent-mode highlight-indentation helm-themes helm-swoop helm-projectile helm-mode-manager helm-make projectile pkg-info epl helm-flx helm-descbinds helm-ag google-translate golden-ratio flx-ido flx fill-column-indicator fancy-battery eyebrowse expand-region exec-path-from-shell evil-visualstar evil-visual-mark-mode evil-unimpaired evil-tutor evil-surround evil-search-highlight-persist evil-numbers evil-nerd-commenter evil-mc evil-matchit evil-lisp-state smartparens evil-indent-plus evil-iedit-state iedit evil-exchange evil-escape evil-ediff evil-args evil-anzu anzu evil goto-chg undo-tree eval-sexp-fu highlight elisp-slime-nav dumb-jump f dash s diminish define-word column-enforce-mode clean-aindent-mode bind-map bind-key auto-highlight-symbol auto-compile packed aggressive-indent adaptive-wrap ace-window ace-link ace-jump-helm-line helm avy helm-core popup async)))
+ '(paradox-github-token t)
+ '(spaceline-helm-mode t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(helm-source-header ((t (:background "color-20" :foreground "white"))))
+ '(highlight ((t (:background "color-25" :foreground "#b2b2b2"))))
+ '(mode-line ((t (:background "color-19" :foreground "#b2b2b2" :box (:line-width 1 :color "#111111")))))
+ '(mode-line-inactive ((t (:background "color-52" :foreground "#b2b2b2" :box (:line-width 1 :color "#111111")))))
+ '(powerline-active1 ((t (:background "color-22" :foreground "#b2b2b2"))))
+ '(powerline-inactive0 ((t (:inherit mode-line-inactive :background "color-61"))))
+ '(powerline-inactive1 ((t (:background "color-56" :foreground "#b2b2b2"))))
+ '(powerline-inactive2 ((t (:background "color-99" :foreground "#b2b2b2"))))
+ '(show-paren-match ((t (:background "green"))))
+ '(show-paren-mismatch ((t (:background "red"))))
+ '(sp-show-pair-enclosing ((t (:inherit highlight :background "color-20"))))
+ '(sp-show-pair-match-face ((t (:inherit bold :background "green" :foreground "#86dc2f" :underline t))))
+ '(sp-show-pair-mismatch-face ((t (:inherit show-paren-mismatch))))
  '(vertical-border ((t (:background "white" :foreground "#111111")))))

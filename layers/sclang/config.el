49214bcc3c243e9ebaf98235603fd4609e330de4
John Grey <jgrey@ucsc.edu>
2018-12-18 09:27:47 +0000

    removed settings that shouldn't be in config

--- layers/sclang/config.el
+++ layers/sclang/config.el
@@ -6,15 +6,3 @@
 ;;(configuration-layer/layer-usedp)
 ;;(configuration-layer/package-usedp)
 
-;;sclang variables:
-(setq-default sclang-auto-scroll-post-buffer t
-              sclang-help-path (quote ("/Applications/SuperCollider/Help"))
-              sclang-program "sclang"
-              sclang-rtf-editor-program "emacs"
-              sclang-show-workspace-on-startup nil
-              show-paren-mode t
-              sclang-library-configuration-file (expand-file-name "~/.spacemacs.d/setup_files/sclang.yaml")
-              sclang-udp-port 57120
-              sclang-eval-line-forward nil
-              sclang-runtime-directory (expand-file-name "~/.sclang/")
-              )

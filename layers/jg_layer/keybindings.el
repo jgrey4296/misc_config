e4baaeb86cf451de844340a2c7faf80b8b415ab6
John Grey <jgrey@ucsc.edu>
2018-12-18 08:05:22 +0000

    added paren shortcuts and register control

--- layers/jg_layer/keybindings.el
+++ layers/jg_layer/keybindings.el
@@ -6,3 +6,18 @@
 ;;(configuration-layer/layer-usedp)
 ;;(configuration-layer/package-usedp)
 
+(global-set-key (kbd "C-c [") 'jg_layer/insert-lparen)
+(global-set-key (kbd "C-c ]") 'jg_layer/insert-rparen)
+(spacemacs/set-leader-keys
+  ;; Registers:
+  "r v" 'view-register
+  "r l" 'list-registers
+  "r w" 'window-configuration-to-register
+  "r x" 'copy-to-register
+  "r j" 'jump-to-register
+  "r f" 'frameset-to-register
+  "r i" 'insert-register
+  ;; Expression:
+  "x e" 'eval-expression
+  )
+

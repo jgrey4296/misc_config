463e2e9f35bfd4f101aeab075754233bfcd8499a
John Grey <jgrey@ucsc.edu>
2018-12-18 23:27:05 +0000

    removed package specific variables to put into package.el

--- layers/jg_layer/config.el
+++ layers/jg_layer/config.el
@@ -4,32 +4,13 @@
 (defun silence ()
   (interactive))
 
-;;(add-hook 'change-major-mode-after-body-hook 'fci-mode)
-;;(neotree-show)
 (xterm-mouse-mode 0)
-(setq-default evil-escape-delay 0.3
+(setq-default 
               ;; AUTO SAVES
               auto-save-file-name-transforms (quote ((".*" "~/.emacs.d/autosaves/`" t)))
               backup-directory-alist (quote ((".*" . "~/.emacs.d/backups/")))
               display-buffer-alist (quote (("*shell*" display-buffer-same-window (nil))))
               icicle-Completions-text-scale-decrease 0
-              ;;ORG SETUP
-              org-agenda-files `(,(expand-file-name "~/github/jg_shell/shellnotes.org"))
-              org-fast-tag-selection-single-key nil
-              org-from-is-user-regexp "\\<John Grey\\>"
-              org-group-tags nil
-              org-use-fast-tag-selection t
-              ;;abbrev-file complaint quieting
-              abbrev-file-name (expand-file-name "~/.spacemacs.d/setup_files/abbrevs_defs")
-
-              ;;setting up erlang
-              ;; (also has a load path set in root el file)
-              erlang-root-dir "/usr/local/opt/erlang"
-              exec-path (cons "/usr/local/opt/erlang/bin" exec-path)
-              ;;yasnippet
-              yas-snippet-dirs `( ,(expand-file-name "~/.spacemacs.d/snippets/")
-                                  ,(expand-file-name "~/github/otherLibs/yasnippet-snippets/snippets")
-                                  ,(expand-file-name "~/github/otherLibs/yasnippet-snippets"))
               ;;personal iSpell dictionary
               ispell-personal-dictionary (expand-file-name "~/.spacemacs.d/setup_files/.ispell_english")
               dired-omit-files "^\\.?#\\|^\\.$\\|^\\.DS_Store$\\|^\\.git$\\|^__pycache__$"
@@ -37,12 +18,10 @@
               ;;use spaces instead of tabs
               tab-width 4
               indent-tabs-mode nil
-              ;;python
-              python-indent-offset 4
-              python-indent-guess-indent-offset nil
               )
 ;; force utf-8
 (set-terminal-coding-system 'utf-8)
 (set-keyboard-coding-system 'utf-8)
 (set-language-environment "UTF-8")
 (prefer-coding-system 'utf-8)
+

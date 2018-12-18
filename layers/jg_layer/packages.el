ae2cf20c2b4d190b97bbe28d6cc5bde3cc2731c5
John Grey <jgrey@ucsc.edu>
2018-12-18 23:30:16 +0000

    added packages, org,yas and abrev bindings

--- layers/jg_layer/packages.el
+++ layers/jg_layer/packages.el
@@ -16,6 +16,10 @@
     abbrev
     evil
     smartparens
+    ibuffer
+    neotree
+    fci
+    rainbow-mode
     )
   )
 
@@ -24,6 +28,7 @@
 ;; (defun <layer>/post-init-<package>)
 
 (defun jg_layer/post-init-evil ()
+  (setq-default evil-escape-delay 0.3 )
   ;; assign colours to hl-line based on state
   (add-hook 'evil-normal-state-entry-hook       (lambda () (set-face-background hl-line-face "#000000")))
   (add-hook 'evil-insert-state-entry-hook       (lambda () (set-face-background hl-line-face "#005f00")))
@@ -45,34 +50,56 @@
 
 
 (defun jg_layer/post-init-org ()
+  ;;ORG SETUP
+  (setq-default
+   org-agenda-files `(,(expand-file-name "~/github/jg_shell/shellnotes.org"))
+   org-fast-tag-selection-single-key nil
+   org-from-is-user-regexp "\\<John Grey\\>"
+   org-group-tags nil
+   org-use-fast-tag-selection t
+   )
   ;; add in keybinding to call tag-occurances
+  (spacemacs/declare-prefix "o" "Org")
+  (spacemacs/declare-prefix "o t" "Tags")
+  (spacemacs/declare-prefix "o a" "Agenda")
+  (spacemacs/declare-prefix "o c" "Calendar")
+  (spacemacs/declare-prefix "o s" "Source")
+  (spacemacs/declare-prefix "o l" "Links")
   (spacemacs/set-leader-keys
     "o T"     'org-todo-list
-    "o t"     'jg_layer/tag-occurances
-    "o v"     'org-tags-view
+    ;; TAGS
+    "o t o"     'jg_layer/tag-occurances
+    "o t v"     'org-tags-view
+    ;; AGENDA
     "o a f"   'org-agenda-file-to-front
     "o a r"   'org-remove-file
-    "o l f"   'list-agenda-files
     "o a l"   'org-agenda-list
     "o a w"   'org-agenda-week-view
     "o a m"   'org-agenda-month-view
-    "o c"     'org-goto-calendar
-    "o d"     'org-date-from-calendar
-    "o D"     'org-time-stamp
+    "o a f"   'list-agenda-files
+    "o a d"   'org-deadline
+    "o a s"     'org-schedule
+    ;; CALENDAR
+    "o c c"     'org-goto-calendar
+    "o c d"     'org-date-from-calendar
+    "o c t"     'org-time-stamp
+    "o c i"     'org-inactive-timestamp
+    ;; SRC CODE
     "o s c"   'org-edit-src-code
+    ;; LINKS
     "o l s"   'org-store-link
     "o l i"   'org-insert-link
     "o l d"   'org-toggle-link-display
-    "o d"     'org-deadline
-    "o t a"   'org-timestamp
-    "o t i"   'org inactive timestamp
-    "o s"     'org-schedule
     )
   )
 
 (defun jg_layer/post-init-yasnippet ()
+  ;;yasnippet
+  (setq-default yas-snippet-dirs `( ,(expand-file-name "~/.spacemacs.d/snippets/")
+                                    ,(expand-file-name "~/github/otherLibs/yasnippet-snippets/snippets")
+                                    ,(expand-file-name "~/github/otherLibs/yasnippet-snippets")))
+  (spacemacs/declare-prefix "y" "Snippets/Abbrevs")
   (spacemacs/set-leader-keys
-    "TAB"    'yas-expand
     "y y"    'yas-expand
     "y i"    'yas-insert-snippet
     "y n"    'yas-new-snippet
@@ -81,6 +108,10 @@
   )
 
 (defun jg_layer/post-init-abbrev ()
+  ;;abbrev-file complaint quieting
+  (setq-default
+   abbrev-file-name (expand-file-name "~/.spacemacs.d/layers/jg_layer/abbrevs_defs")
+   )
   (spacemacs/set-leader-keys
     "y e"  'edit-abbrevs
     "y w"  'write-abbrev-file
@@ -89,10 +120,39 @@
     "y A"  'add-mode-abbrev
     "y k"  'kill-all-abbrevs
     )
+  (global-set-key (kbd "TAB") 'expand-abbrev)
   )
 
-(defun jg/layer/post-smartparens ()
+(defun jg_layer/post-smartparens ()
   (setq-default smartparens-global-mode 0
-
                 )
+  )
+
+(defun jg_layer/post-ibuffer ()
+  )
+
+(defun jg_layer/post-erlang ()
+  ;; (also has a load path set in root el file)
+  erlang-root-dir "/usr/local/opt/erlang"
+  exec-path (cons "/usr/local/opt/erlang/bin" exec-path)
+
+  )
+
+(defun jg_layer/post-python ()
+  (setq-default python-indent-offset 4
+                python-indent-guess-indent-offset nil )
+  )
+
+(defun jg_layer/post-fci ()
+  (add-hook 'change-major-mode-after-body-hook 'fci-mode)
+  )
+
+(defun jg_layer/init-rainbow-mode ()
+  (use-package rainbow-mode
+    :commands (rainbow-mode)
+    )
+  )
+
+(defun jg_layer/post-init-rainbow-mode ()
+  (rainbow-mode t)
   )

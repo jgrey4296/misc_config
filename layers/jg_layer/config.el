bb3ada489e8864757c26964d24395b36454c1c8b
John Grey <jgrey@ucsc.edu>
2019-02-01 17:15:33 +0000

    whitespace changes

--- layers/jg_layer/config.el
+++ layers/jg_layer/config.el
@@ -3,19 +3,19 @@
 
 (xterm-mouse-mode 0)
 (setq-default 
-              ;; AUTO SAVES
-              auto-save-file-name-transforms (quote ((".*" "~/.emacs.d/autosaves/`" t)))
-              backup-directory-alist (quote ((".*" . "~/.emacs.d/backups/")))
-              display-buffer-alist (quote (("*shell*" display-buffer-same-window (nil))))
-              icicle-Completions-text-scale-decrease 0
-              ;;personal iSpell dictionary
-              ispell-personal-dictionary (expand-file-name "~/.spacemacs.d/setup_files/.ispell_english")
-              dired-omit-files "^\\.?#\\|^\\.$\\|^\\.DS_Store$\\|^\\.git$\\|^__pycache__$"
+ ;; AUTO SAVES
+ auto-save-file-name-transforms (quote ((".*" "~/.emacs.d/autosaves/`" t)))
+ backup-directory-alist (quote ((".*" . "~/.emacs.d/backups/")))
+ display-buffer-alist (quote (("*shell*" display-buffer-same-window (nil))))
+ icicle-Completions-text-scale-decrease 0
+ ;;personal iSpell dictionary
+ ispell-personal-dictionary (expand-file-name "~/.spacemacs.d/setup_files/.ispell_english")
+ dired-omit-files "^\\.?#\\|^\\.$\\|^\\.DS_Store$\\|^\\.git$\\|^__pycache__$"
 
-              ;;use spaces instead of tabs
-              tab-width 4
-              indent-tabs-mode nil
-              )
+ ;;use spaces instead of tabs
+ tab-width 4
+ indent-tabs-mode nil
+ )
 ;; force utf-8
 (set-terminal-coding-system 'utf-8)
 (set-keyboard-coding-system 'utf-8)
@@ -26,3 +26,7 @@
   "s o" 'occur
   )
 
+(eval-after-load 'dired-mode
+  (add-hook 'dired-mode-hook 'dired-omit-mode)
+  ;;(setq dired-listing-switches "-alh")
+)

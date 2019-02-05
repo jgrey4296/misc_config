2db3582e4ec4dc69fc161e06717cc297609c7f78
John Grey <jgrey@ucsc.edu>
2019-02-05 13:45:09 +0000

    moved keys to a better place

--- layers/jg_layer/config.el
+++ layers/jg_layer/config.el
@@ -2,7 +2,7 @@
 ;; loaded fourth
 
 (xterm-mouse-mode 0)
-(setq-default 
+(setq-default
  ;; AUTO SAVES
  auto-save-file-name-transforms (quote ((".*" "~/.emacs.d/autosaves/`" t)))
  backup-directory-alist (quote ((".*" . "~/.emacs.d/backups/")))
@@ -21,10 +21,6 @@
 (set-keyboard-coding-system 'utf-8)
 (set-language-environment "UTF-8")
 (prefer-coding-system 'utf-8)
-(spacemacs/set-leader-keys
-  "t o" 'dired-omit-mode
-  "s o" 'occur
-  )
 
 (eval-after-load 'dired-mode
   (add-hook 'dired-mode-hook 'dired-omit-mode)

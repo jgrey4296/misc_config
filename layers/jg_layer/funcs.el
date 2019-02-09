f718dccefb85a9e1213d10cf297092f8b6330e15
John Grey <jgrey@ucsc.edu>
2019-02-09 15:36:22 +0000

    transient and helm example code

--- layers/jg_layer/funcs.el
+++ layers/jg_layer/funcs.el
@@ -404,7 +404,39 @@
 
 
 (defun jg_layer/clear-buffer ()
-    " from https://stackoverflow.com/questions/24565068/ "
+  """ Utility to clear a buffer
+    from https://stackoverflow.com/questions/24565068/ """
   (interactive)
   (let ((inhibit-read-only t)) (erase-buffer))
-    )
+  )
+
+;;----------------------------------------
+(defun jg_layer/example_transient_func ()
+  (interactive)
+  (print "blah")
+  )
+
+(spacemacs|define-transient-state example_transient
+  :title "an example transient state"
+  :doc (concat "
+[_q_]^^ Quit         [_h_]^^ Example Func
+[_t_]^^ Example Func
+")
+  :bindings
+  ("q" nil :exit t)
+  ("t" jg_layer/example_transient_func)
+  ("h" jg_layer/example_transient_func :exit t)
+  )
+
+(spacemacs/set-leader-keys "." 'spacemacs/example_transient-transient-state/body)
+
+
+;;----------------------------------------
+;; (setq-default jg_layer/example_helm-source
+;;               (helm-make-source "Find Image" 'helm-source-ffiles
+;;                 :action '(("action" . (lambda (candidate) (insert candidate))))))
+
+;; (defun jg_layer/example_helm-call ()
+;;   (interactive)
+;;   (helm :sources jg_layer/example_helm-image-helm-source
+;;         :input "./"))

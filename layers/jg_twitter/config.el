1c70ffbc9a1936a13698333d9260b06bf94bc5df
John Grey <jgrey@ucsc.edu>
2019-02-18 03:55:04 +0000

    a simple layer to tweet and upload images

---
+++ layers/jg_twitter/config.el
@@ -0,0 +1,18 @@
+ (setq-default jg_twitter/twurl_buff_name "*twurl*"
+              jg_twitter/twurl_proc_name "*twurl-proc*"
+              jg_twitter/twurl_media_host "upload.twitter.com"
+              jg_twitter/twurl_default_host "api.twitter.com"
+              jg_twitter/twurl_upload "/1.1/media/upload.json"
+              jg_twitter/twurl_tweet "/1.1/statuses/update.json"
+              jg_twitter/twurl_file nil
+              jg_twitter/twurl_media_id nil
+              jg_twitter/twurl_target_tweet nil
+              jg_twitter/tweet_buff_name "*tweet*")
+
+(with-eval-after-load 'helm-source
+  (with-eval-after-load 'helm-files
+    (setq-default jg_twitter/twitter-image-helm-source
+                  (helm-make-source "Find Image" 'helm-source-ffiles
+                    :action '(("action" . jg_twitter/twitter-upload-image))))
+    )
+)
---
+++ layers/jg_twitter/packages.el
@@ -0,0 +1,16 @@
+(defconst jg_twitter-packages
+  '(
+    tramp
+    evil
+    )
+  )
+
+(defun jg_twitter/init-tramp ()
+  (use-package tramp)
+  )
+
+(defun jg_twitter/post-init-evil ()
+  (evil-ex-define-cmd "tweet" 'jg_twitter/tweet)
+  (evil-ex-define-cmd "image" 'jg_twitter/twitter-add-picture)
+  )
+

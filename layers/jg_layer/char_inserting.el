369c002237c2ff12b4b8610dc0b33401c56204b5
John Grey <jgrey@ucsc.edu>
2019-03-10 18:02:16 +0000

    switched to allow multiple candidates

--- layers/jg_layer/char_inserting.el
+++ layers/jg_layer/char_inserting.el
@@ -33,7 +33,8 @@
 
   (setq jg_layer/char-inserting-candidates-pairs (jg_layer/char-inserting-get-candidates)
         jg_layer/char-inserting-helm `((name . "Helm Insert Char")
-                                       (action . (lambda (cand) (insert-char (car cand))))
+                                       (action . (lambda (cand)
+                                                   (mapcar (lambda (x) (insert-char (car x))) (helm-marked-candidates))))
                                        )
         )
 

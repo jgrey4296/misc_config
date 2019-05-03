7399c312f944ebe9500f406503efa6bc702b05a8
John Grey <jgrey@ucsc.edu>
2019-05-03 01:43:16 +0100

    adds sequence mode to  trie packages

--- layers/trie/packages.el
+++ layers/trie/packages.el
@@ -9,8 +9,10 @@
     ;; (some-package :location local)
     ;; (some-package :location (recipe :fetcher github :repo "some/repo"))
     ;; (some-package :excluded t)
+    org
     (trie-mode :location local)
     (parsec :location elpa :step pre)
+    (sequence-mode :location local)
     )
   )
 
@@ -25,3 +27,20 @@
 (defun trie/init-parsec ()
   (use-package parsec
     :defer t))
+
+
+(defun trie/init-sequence-mode ()
+  (use-package sequence-mode
+    :config
+    (spacemacs/declare-prefix "," "Sequence Mode Prefix")
+    (evil-define-key '(normal visual) sequence-mode-map
+      "l" 'sequence/user-inc-column
+      "h" 'sequence/user-dec-column
+      "k" 'sequence/user-dec-line
+      "j" 'sequence/user-inc-line
+      )
+    (spacemacs/set-leader-keys-for-major-mode 'sequence-mode
+      "."   'spacemacs/sequence_transient-transient-state/body
+      )
+    )
+)

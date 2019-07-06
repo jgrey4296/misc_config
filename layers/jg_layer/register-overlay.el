5cda39a4223e8babf7fd223126fb310a982a8540
John Grey <jgrey@ucsc.edu>
2019-07-06 18:03:24 +0100

    adds a stub for overlay management

---
+++ layers/jg_layer/register-overlay.el
@@ -0,0 +1,24 @@
+(require 'dash)
+
+;; Manage faux-registers that allow custom hiding and highlighting of text
+
+(defun jg_layer/register-overlay-setup ()
+  ;; make buffer-local overlays
+
+  )
+
+
+
+(defun jg_layer/register-overlay-add ()
+
+
+  )
+
+(defun jg_layr/register-overlay-clear ()
+
+  )
+
+(defun jg_layer/register-overlay-toggle ()
+
+
+  )

b000bdc035ef841ad9d714cd2f69d8079468432e
John Grey <jgrey@ucsc.edu>
2019-08-22 14:39:15 +0100

    removes obsolete char insertion

--- layers/char-insertion/config.el
+++
@@ -1,7 +0,0 @@
-(setq jg_layer/char-inserting-candidates-pairs (jg_layer/char-inserting-get-candidates)
-      jg_layer/char-inserting-helm `((name . "Helm Insert Char")
-                                     (action . (lambda (cand)
-                                                 (mapcar (lambda (x) (insert-char (car x))) (helm-marked-candidates))))
-                                     )
-      )
-

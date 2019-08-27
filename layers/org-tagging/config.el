29307a34624c4ca29268203aa4d3d1ea06f196c1
John Grey <jgrey@ucsc.edu>
2019-08-27 02:08:53 +0100

    adds the packages setup for org-tagging minor mode

--- layers/org-tagging/config.el
+++ layers/org-tagging/config.el
@@ -3,11 +3,10 @@
       ;; Start Position -> End Line number because of changes in positions from tag add/retract
       org-tagging/org-tagging-region '()
       org-tagging/org-tagging-helm `((name . "Helm Tagging")
-                                  (action . (("set" . org-tagging/org-set-tags)))
-                                  )
+                                     (action . (("set" . org-tagging/org-set-tags)))
+                                     )
       org-tagging/org-tagging-fallback-source `((name . "")
                                                 (action . (("Create" . org-tagging/org-set-new-tag)))
-                                                (filtered-candidate-transformer
-                                                 (lambda (_c _s) (list helm-pattern)))
+                                                (filtered-candidate-transformer (lambda (_c _s) (list helm-pattern)))
                                                 )
  )

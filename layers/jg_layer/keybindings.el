5d52e3830ef7f4e12dd226aef1a8efd4200b370a
John Grey <jgrey@ucsc.edu>
2019-08-27 02:07:38 +0100

    removes keybindings from jg_layer

--- layers/jg_layer/keybindings.el
+++ layers/jg_layer/keybindings.el
@@ -9,37 +9,5 @@
 (global-set-key (kbd "C-c [") 'jg_layer/insert-lparen)
 (global-set-key (kbd "C-c ]") 'jg_layer/insert-rparen)
 (spacemacs/declare-prefix "x i" "Indent")
-(spacemacs/set-leader-keys
-  ;; Registers:
-  "r v" 'view-register
-  "r l" 'list-registers
-  "r w" 'window-configuration-to-register
-  "r x" 'copy-to-register
-  "r j" 'jump-to-register
-  "r f" 'frameset-to-register
-  "r i" 'insert-register
-  ;; Expression:
-  "x e" 'eval-expression
-  ;; Clearing
-  "b c" 'jg_layer/clear-buffer
-  ;;Indirect buffer
-  "b i" 'clone-indirect-buffer-other-window
-  ;; Indenting
-  "x i c" 'indent-to-column
-  ;; toggles
-  "t o" 'dired-omit-mode
-  ;; searching
-  "s o" 'helm-occur
-  ;; mode select
-   "m"   'helm-switch-major-mode
-  ;; mark buffer
-  "x m" 'mark-whole-buffer
-  ;; Access to old school emacs help:
-  "h h" 'help
-  ;; ac trigger
-  ;;"a a" 'jg_layer/ac-trigger
-  ;; goto org agenda file
-  "b a" 'jg_layer/goto-org-agenda-file
-  ;; Char Insertion
-  "x C" 'insert-char
-)
+(spacemacs/declare-prefix "a m" "Modes")
+(spacemacs/declare-prefix "a f" "Free Keys")

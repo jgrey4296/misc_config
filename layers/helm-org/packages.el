b68c0b6ef386baf6d6c84bfd3e08d447d62fb765
John Grey <jgrey@ucsc.edu>
2019-08-28 01:15:50 +0100

    improves helm-org layer

--- layers/helm-org/packages.el
+++ layers/helm-org/packages.el
@@ -1,18 +1,13 @@
 (defconst helm-org-packages
   '(
     helm-org
+    org
     )
 )
 
 (defun helm-org/init-helm-org ()
   (use-package helm-org
     :defer t
-    :init
-    (progn
-      (evil-define-key 'normal org-mode-map
-        (kbd "g h")     'helm-org-in-buffer-headings
-	      )
-      )
     :config
     (progn
       (add-to-list 'helm-completing-read-handlers-alist '(org-capture . helm-org-completing-read-tags))
@@ -21,3 +16,9 @@
     )
   )
 
+(defun helm-org/post-init-org ()
+  (add-hook 'org-mode-hook (lambda ()
+                             (evil-define-key 'normal org-mode-map
+                               (kbd "g h")     'helm-org-in-buffer-headings
+	                             )))
+  )

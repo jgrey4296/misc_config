9e8dc4271fc1ba511ac790f10c712d2b2c074197
John Grey <johngrey4296@gmail.com>
2019-09-03 16:28:06 +0100

    adds a variable for preferred org linecount size

--- layers/tag-unify/config.el
+++ layers/tag-unify/config.el
@@ -1,5 +1,6 @@
 (setq-default tag-unify/loc-bookmarks "/Users/jgrey/github/writing/resources/main_bookmarks.html"
               tag-unify/loc-bibtex "~/github/writing/resources/years"
+              tag-unify/preferred-linecount-for-org 1500
               tag-unify/loc-master-tag-list ""
               tag-unfiy/org-clean-marker nil
               bibtex-completion-bibliography nil

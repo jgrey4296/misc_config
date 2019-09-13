80e167871e3dcc6c1b6187a1a42ffa92774c2727
John Grey <johngrey4296@gmail.com>
2019-09-13 23:45:37 +0100

    created a dedicated logic layer

---
+++ layers/logic/packages.el
@@ -0,0 +1,34 @@
+(defconst ccalc-packages
+  '(
+    ob-prolog
+    (ob-ccalc :location local)
+    (ob-clingo :location local)
+    )
+)
+
+
+(defun logic/init-ob-ccalc ()
+  (use-package ob-ccalc
+    :init
+    (org-babel-do-load-languages
+     'org-babel-load-languages '((ccalc . t)))
+    (push '("ccalc" . prolog) org-src-lang-modes)
+    )
+  )
+(defun logic/init-ob-clingo ()
+  (use-package ob-clingo
+    :init
+    (org-babel-do-load-languages
+     'org-babel-load-languages '((clingo . t)))
+    (push '("clingo" . prolog) org-src-lang-modes)
+    )
+
+  )
+(defun logic/init-ob-prolog ()
+  (use-package ob-prolog
+    :init
+    (org-babel-do-load-languages
+     'org-babel-load-languages '((prolog . t)))
+    )
+  )
+

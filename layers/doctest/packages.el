ee069b4c8223dfdef3a05bf1a6aacd53dcb52ad0
John Grey <johngrey4296@gmail.com>
2019-09-23 00:53:43 +0100

    adds keybinding for running doctest

--- layers/doctest/packages.el
+++ layers/doctest/packages.el
@@ -4,3 +4,17 @@
     parsec
     )
   )
+
+(defun doctest/init-parsec ()
+  (use-package parsec
+    :commands (parsec-with-input)
+    :defer t)
+  )
+
+(defun doctest/post-init-org ()
+  (defun doctest/org-mod-map ()
+    (spacemacs/set-leader-keys-for-major-mode 'org-mode
+      ". T" 'doctest/test-org-file)
+    )
+  (add-hook 'org-mode-hook 'doctest/org-mod-map)
+  )

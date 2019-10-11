43871d6cbcba7e37c4faef34fd930225a15f35ce
John Grey <johngrey4296@gmail.com>
2019-10-11 03:48:26 +0100

    activates and keybinds overlay control hydra

--- layers/overlay_control/packages.el
+++ layers/overlay_control/packages.el
@@ -2,16 +2,40 @@
   '(
     helm
     (overlay-ctrl-minor-mode :location local)
+    (font-lock+ :location (recipe :fetcher git :url "https://github.com/emacsmirror/font-lock-plus"))
     )
   )
 
 (defun overlay_control/init-overlay-ctrl-minor-mode ()
   (use-package overlay-ctrl-minor-mode
-    :commands ()
+    :commands (overlay-ctrl-minor-mode global-overlay-ctrl-mode)
     :config
+    (spacemacs|define-transient-state overlay-apply
+      :title "Overlay Application"
+      :doc (concat "
+    | Overlay Commands                 ^^|
+    |----------------------------------^^|--------------------------
+    | [_q_] Quit                         | [_r_] Overlay Region
+    | [_a_] Add To Register              | [_w_] Overlay Word
+    | [_b_] Apply Register To Buffer     | [_h_] Hide-Overlay
+    | [_c_] Clear Register               | [_C_] Clear Overlays
+    |----------------------------------^^---------------------------
+    | [_d_] Set Overlay Type             | [_D_] Set Hide Text
+")
+      :bindings
+      ("q" overlay_control/re-run-overlays :exit t)
+      ("a" overlay_control/add-to-register)
+      ("b" overlay_control/apply-overlay-register)
+      ("C" overlay_control/clear-on-buffer)
+      ("c" overlay_control/clear-register)
+      ("d" overlay_control/overlay-helm)
+      ("r" overlay_control/apply-to-region)
+      ("h" overlay_control/apply-hide-overlay)
+      ("D" overlay_control/set-hide-text)
+      ("w" overlay_control/overlay-word)
+      )
     (spacemacs/set-leader-keys
-      "s ." 'overlay_control/overlay-apply-hydra
-      "r o" 'overlay_control/overlay-register-hydra
+      "a /" 'spacemacs/overlay-apply-transient-state/body
       )
     )
   )
@@ -20,6 +44,22 @@
   (spacemacs|use-package-add-hook helm
     :post-config
     ;; build helm for selecting overlay type
-
+    (setq overlay_control/overlay-helm-source
+          (helm-make-source "Overlay Helm" 'helm-source-sync
+            :action (helm-make-actions "Set Next Overlay" 'overlay_control/set-overlay)
+            :candidates overlay_control/helm-candidates
+            :nomark t
+            )
+          )
+    (defun overlay_control/overlay-helm ()
+      (interactive)
+      (helm :sources overlay_control/overlay-helm-source
+            :buffer "*Helm Overlays*"
+            )
+      )
     )
+  )
+
+(defun overlay_control/init-font-lock+ ()
+  (use-package font-lock+)
   )

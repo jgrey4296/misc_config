b537e986ee49e802ea5e5e3b452c98c8abc73634
John Grey <johngrey4296@gmail.com>
2019-10-11 03:48:00 +0100

    implements the core overlay_control minor mode

--- layers/overlay_control/local/overlay-ctrl-minor-mode/overlay-ctrl-minor-mode.el
+++ layers/overlay_control/local/overlay-ctrl-minor-mode/overlay-ctrl-minor-mode.el
@@ -1,8 +1,16 @@
-(define-minor-mode overly-ctrl-mode
-  "A Minor mode for generalised SCLang/Tidal/Chuck commands"
-  :lighter "Music"
+(define-minor-mode overlay-ctrl-minor-mode
+  "Minor Mode to handle overlay controls"
+  :init-value nil
+  :lighter "OvCtrl"
+  (defvar-local overlay_control/overlays '() "Buffer Local Overlays")
   )
 
-;; (define-globalized-minor-mode global-music-mode music-minor-mode music-on)
+(defun overlay-ctrl-on ()
+  (unless (minibufferp)
+    (overlay-ctrl-minor-mode 1)
+    )
+  )
+
+(define-globalized-minor-mode global-overlay-ctrl-mode overlay-ctrl-minor-mode overlay-ctrl-on)
 
 (provide 'overlay-ctrl-minor-mode)

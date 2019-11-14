d33868ce2b8fcbd0939ee2a919696892d5dc48e1
jg <johngrey@Johns-Mac-mini.local>
2019-11-14 02:28:46 +0000

    updates trie mode to font lock better

--- layers/trie/local/trie-mode/trie-mode.el
+++ layers/trie/local/trie-mode/trie-mode.el
@@ -1,7 +1,9 @@
-; based On https://www.emacswiki.org/emacs/ModeTutorial
+                                        ; based On https://www.emacswiki.org/emacs/ModeTutorial
 ;;For allowing code to run when the mode is run:
 (require 'dash)
+(require 'trie-face-macro)
 
+(defgroup trie-mode '() "Trie Mode Customizations")
 ;;--------------------
 ;; Mode Variables
 ;;--------------------
@@ -13,28 +15,26 @@
 ;;--------------------
 ;;definitions
 ;;--------------------
-(defconst trie-keywords '("assert" "retract"))
-
+(defconst trie-keywords '("assert" "retract" "end"))
 ;;generate regexp for keywords
-(defconst trie-keywords-regexp (regexp-opt trie-keywords 'words))
-
+(defconst trie-keywords-regexp (regexp-opt trie-keywords))
 
 ;;--------------------
 ;;face definitions
 ;; use them as symbols 'blah in font-lock-keywords
 ;;--------------------
 (defface trie-rulename
-    '((t
-       :foreground "red"
-       :background "black"
-       :underline t))
+  '((t
+     :foreground "green"
+     :background "black"
+     :underline t))
   "Face for Rule names"
   :group 'trie-mode)
 (defface trie-ruleend
-    '((t
-       :foreground "red"
-       :background "black"
-       ))
+  '((t
+     :foreground "red"
+     :background "black"
+     ))
   "Face for the end of rules"
   :group 'trie-mode)
 
@@ -58,26 +58,25 @@
 (defconst trie-font-lock-keywords
   (list
    ;;facts
-   '("\\(\\.[$.!a-zA-Z0-9_\]+\\((.*)\\)?\\)+\\(\\?$\\)?"
-     (0 (trie-depth-face 9) t)
-     (2 (trie-depth-face 6) t t)
-     (3 (trie-depth-face 1) t t))
-   ;;Rule name
-   '("\\(\\..+\\)+:$" (0 (trie-depth-face 0) t))
-   ;;Rule end
-   `("end$" (0 (trie-depth-face 0)))
-   ;;Transform
-   '("\\(\\$[a-zA-Z0-9_]+ [+*/-] [$a-zA-Z0-9_.]+\\( ?-> ?\\$[a-zA-Z0-9_]+\\)?\\)"
-     (0 font-lock-constant-face t))
-   ;;Actions
-   '("\\([a-zA-Z+@-]+( ?\\)\\(\\(.\\)+\\)\\( ?)\\)"
-     (1 (trie-depth-face 6) t)
-     (2 (trie-depth-face 9) )
-     (4 (trie-depth-face 6) t))
-   ;;Variables
-   '("\\$[a-zA-Z0-9_]+" (0 (trie-depth-face 5) t))
+   ;; '("\\([$.!a-zA-Z0-9_\]+\\((.*)\\)?\\)+\\(\\?$\\)?"
+   ;;   (0 (trie-depth-face 9) t)
+   ;;   (2 (trie-depth-face 6) t t)
+   ;;   (3 (trie-depth-face 1) t t))
    ;;Exclusion op
-   '("!" (0 "trie-rulename" t))
+   ;; '("!" (0 (trie-depth-face 4) t))
+   ;;Rule name
+   `("\\(.+\\)+:$" (0 "trie-rulename" t))
+   ;; ;;Rule end
+   ;; '("end$" (0 (trie-depth-face 0)))
+   ;;Transform and Query
+   `("->\\|\\?" (0 ,(trie-depth-face 4) t))
+   ;;Actions
+   ;; '("\\([a-zA-Z+@-]+( ?\\)\\(\\(.\\)+\\)\\( ?)\\)"
+   ;;   (1 (trie-depth-face 6) t)
+   ;;   (2 (trie-depth-face 9) )
+   ;;   (4 (trie-depth-face 6) t))
+   ;;Variables and tags
+   `("[\\$#][a-zA-Z0-9_]+" (0 ,(trie-depth-face 2)))
    )
   "Minimal highlighting expressions for trie mode")
 
@@ -91,39 +90,40 @@
 (defun trie-indent-line()
   "Indent current-line as trie code"
   (interactive)
+  (message "Indenting")
   (beginning-of-line)
   (if (bobp) ;;if at beginning of buffer
-       ;;then:
+      ;;then:
       (indent-line-to 0)
-      ;;else:
-      (let ((not-indented t) cur-indent)
-        (if (looking-at "^[ \t]*end$") ;;if line contains an END_
-            (progn
-              (save-excursion ;;save where we are
-                (forward-line -1) ;;go back a line
-                ;;then deindent:
-                (setq cur-indent (- (current-indentation) tab-width))
-                ;;guard:
-                (if (< cur-indent 0)
-                    (setq cur-indent 0))))
-            ;;else:
-            (save-excursion
-              (while not-indented
-                (forward-line -1)
-                (if (looking-at "^[ \t]*end$")
-                    (progn
-                      (setq cur-indent (current-indentation))
-                      (setq not-indented nil))
-                    ;;otherwise
-                    (if (looking-at "^.+:")
-                        (progn
-                          (setq cur-indent (+ (current-indentation) tab-width))
-                          (setq not-indented nil))
-                        (if (bobp)
-                            (setq not-indented nil)))))))
-        (if cur-indent
-            (indent-line-to cur-indent)
-            (indent-line-to 0)))))
+    ;;else:
+    (let ((not-indented t) cur-indent)
+      (if (looking-at "^[ \t]*end$") ;;if line contains an END_
+          (progn
+            (save-excursion ;;save where we are
+              (forward-line -1) ;;go back a line
+              ;;then deindent:
+              (setq cur-indent (- (current-indentation) tab-width))
+              ;;guard:
+              (if (< cur-indent 0)
+                  (setq cur-indent 0))))
+        ;;else:
+        (save-excursion
+          (while not-indented
+            (forward-line -1)
+            (if (looking-at "^[ \t]*end$")
+                (progn
+                  (setq cur-indent (current-indentation))
+                  (setq not-indented nil))
+              ;;otherwise
+              (if (looking-at "^.+:")
+                  (progn
+                    (setq cur-indent (+ (current-indentation) tab-width))
+                    (setq not-indented nil))
+                (if (bobp)
+                    (setq not-indented nil)))))))
+      (if cur-indent
+          (indent-line-to cur-indent)
+        (indent-line-to 0)))))
 
 ;;--------------------
 ;;Syntax Table
@@ -158,7 +158,11 @@
   (interactive)
   (kill-all-local-variables)
   (use-local-map trie-mode-map)
-  (set (make-local-variable 'font-lock-defaults) '(trie-font-lock-keywords))
+  (let ((base-locks (reverse trie-font-lock-keywords))
+        (keywords (list trie-keywords-regexp 0 "trie-rulename" t)))
+    (push keywords base-locks)
+    (set (make-local-variable 'font-lock-defaults) (list (reverse base-locks)))
+    )
   (set (make-local-variable 'indent-line-function) 'trie-indent-line)
   (set (make-local-variable 'comment-style) '(plain))
   (set (make-local-variable 'comment-start) "//")
@@ -167,7 +171,7 @@
   (setq major-mode 'trie-mode)
   (setq mode-name "TRIE")
   (run-mode-hooks)
-)
+  )
 
 ;;todo later: set no longer needed variables to nil
 

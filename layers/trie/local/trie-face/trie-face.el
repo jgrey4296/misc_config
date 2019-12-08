844246c4f2ef7c9146f7d559a70e1cacb1bce4d8
John Grey <johngrey4296@gmail.com>
2019-12-08 19:53:05 +0000

    moved trie-face macro code to separate package

--- layers/trie/local/trie-face/trie-face.el
+++ layers/trie/local/trie-face/trie-face.el
@@ -1,15 +1,16 @@
 ;; Macros for generating faces programmatically for trie mode
 (require 'dash)
 
-(defcustom trie-defined-depth-count 10 "Number of Trie Faces to Generate")
-(defcustom trie-outermost-depth-face-count 3 "Generated Trie Faces depth")
-(defcustom trie-depth-face-name "trie-depth-" "Generated Trie Face Name Base")
+(defgroup trie-face-generator '() "Variables of the macro based face generation")
+(defcustom trie-defined-depth-count 10 "Number of Trie Faces to Generate" :type '(integer))
+(defcustom trie-outermost-depth-face-count 3 "Generated Trie Faces depth" :type '(integer))
+(defcustom trie-depth-face-name "trie-depth-" "Generated Trie Face Name Base" :type '(string))
 (defcustom trie-depth-color-list '("color-26" "color-47" "color-99" "color-124"
                                    "color-129" "color-142" "color-164")
-  "Default Generate Trie Face Colours")
+  "Default Generate Trie Face Colours" :type '(sexp))
 
 
-(defun flatten (lst)
+(defun trie-face/flatten (lst)
   (letrec ((internal (lambda (x)
                        (cond
                         ((null x) nil)
@@ -20,7 +21,8 @@
       (assert (listp lst))
       (funcall internal lst))))
 
-(defun make-list-as-big-as-n (lst n)
+(defun trie-face/make-list-as-big-as-n (lst n)
+  " Repeat a list up to size n"
   (progn
     (assert (listp lst))
     (assert (numberp n))
@@ -28,16 +30,16 @@
       (if (>= lenlst n)
           lst
         (letrec ((repN (+ 1 (/ n lenlst)))
-                 (newLst (flatten (-repeat repN lst))))
+                 (newLst (trie-face/flatten (-repeat repN lst))))
           (assert (>= (length newLst) n))
           newLst)))))
 
-(defun findFace (name)
+(defun trie-face/findFace (name)
   (seq-find (lambda (x) (string= (face-name x) name)) (face-list)))
 
 ;;adapted from rainbow-blocks.el
 ;;returns a string of a face name
-(defun trie-depth-face (depth)
+(defun trie-face/trie-depth-face (depth)
   (let ((name (concat trie-depth-face-name
                       (number-to-string
                        (or
@@ -50,21 +52,21 @@
                                    trie-outermost-depth-face-count))))))))
     name))
 
-(defmacro* trie-generate-face (name color)
+(defmacro* trie-face/trie-generate-face (name color)
   `(defface ,name
        (list (list t :foreground ,color)) ;; :background "black"))
      "A Generated Face"
      :group 'trie-mode))
 
 ;;Create depth faces:
-(defun trie-face-creation ()
+(defun trie-face/trie-face-creation ()
   (letrec (
            ;;create the range
            (range (number-sequence 0 (- trie-defined-depth-count 1)))
            ;;create the names
-           (names (map 'sequence 'trie-depth-face range))
+           (names (map 'sequence 'trie-face/trie-depth-face range))
            ;;Setup the color list at correct list length
-           (colorList (make-list-as-big-as-n trie-depth-color-list
+           (colorList (trie-face/make-list-as-big-as-n trie-depth-color-list
                                              trie-defined-depth-count))
            ;;pair with a foreground color
            (paired (-zip-pair names colorList)))
@@ -76,8 +78,8 @@
           ;; (message "Creating Face: %s" newFaceName)
           ;; (message (format "%S" (macroexpand `(trie-generate-face ,(intern newFaceName)
           ;;                                                       newFaceColor))))
-          (eval (macroexpand `(trie-generate-face ,(intern newFaceName) newFaceColor))))))))
+          (eval (macroexpand `(trie-face/trie-generate-face ,(intern newFaceName) newFaceColor))))))))
 
-(trie-face-creation)
+(trie-face/trie-face-creation)
 
-(provide 'trie-face-macro)
+(provide 'trie-face)

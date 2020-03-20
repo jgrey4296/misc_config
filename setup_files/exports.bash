8a7c754151f26d623d06e78cebe438df729ca2c3
John Grey <johngrey4296@gmail.com>
2020-03-20 15:02:35 +0000

    remove obsolete paths

--- setup_files/exports.bash
+++ setup_files/exports.bash
@@ -1,9 +1,9 @@
 #PATHS:
 export PATH=/sbin:/usr/sbin:/bin:/usr/bin
+export PATH=/usr/local/sbin:$PATH
 export PATH=/usr/local/bin:$PATH
 export PATH=/Applications/SuperCollider:$PATH
 export PATH=/usr/local/opt/coreutils/libexec/gnubin:$PATH
-export PATH=/Applications/Racket\ v6.1.1/bin:$PATH
 export PATH=/usr/local/go/bin:$PATH
 
 #Node:
@@ -13,20 +13,11 @@ export PATH=~/.npm-global/bin:$PATH
 export PATH=/Applications/SuperCollider/SuperCollider.app/Contents/Resources:$PATH
 export PATH=/Applications/SuperCollider/SuperCollider.app/Contents/MacOS:$PATH
 
-#racket:
-export PATH=/Applications/Racket/bin:$PATH
-
 #for latex (mactex, when installed):
-export PATH=/usr/local/texlive/2017/bin/x86_64-darwin:$PATH
+# export PATH=/usr/local/texlive/2017/bin/x86_64-darwin:$PATH
 
 #for blender:
 export PATH=/Applications/blender/blender.app/Contents/MacOS:$PATH
-
-#For clingo:
-export PATH=~/clingo:$PATH
-
-#for haskell / cabal
-export PATH=$HOME/Library/Haskell/bin:$PATH
 
 #for jacamo (see lang specific)
 export PATH=$JACAMO_HOME/scripts:$PATH

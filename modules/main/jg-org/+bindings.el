c9ab2be47aba11e4630a8238d138a263f72b26eb
John Grey <johngrey4296@gmail.com>
2020-09-21 18:24:37 +0100

    move jg-org bindings into separate file

---
+++ modules/main/jg-org/+bindings.el
@@ -0,0 +1,60 @@
+;;; main/jg-org/+bindings.el -*- lexical-binding: t; -*-
+
+(map! :leader
+        (:prefix ("xA" . "Academic Phrases")
+        "p" #'academic-phrases
+        "s" #'academic-phrases-by-section)
+        ;; AGENDA
+        (:prefix ("ao" . "Org")
+         (:prefix ("a" . "Agenda")
+          "/"   'org-occur-in-agenda-files
+          "f"   'org-agenda-file-to-front
+          "r"   'org-remove-file
+          "l"   'org-agenda-list
+          "F"   '+jg-org-list-agenda-files
+          "t"   'org-tags-view)
+         )
+        (:prefix "i"
+         "t" #'org-time-stamp)
+        (:prefix "j"
+         "c" #'org-goto-calendar)
+        "a U r" #'+jg-org-bibtex-load-random)
+
+(map! :after org
+      :map org-mode-map
+        "C-c [" nil
+        "C-c ]" nil
+        )
+;; (map! :after org
+;;       :map org-agenda-mode-map
+;; 	:prefix "v"
+;;         "w"   #'org-agenda-week-view
+;;         "m"   #'org-agenda-month-view
+;;         )
+
+(map! :after org
+      :mode org-mode
+        :n "g j" #'org-forward-heading-same-level
+        :n "g k" #'org-backward-heading-same-level
+        :n "] p" #'org-next-link
+        :n "[ p" #'org-previous-link
+        :n  "g h" #'helm-org-in-buffer-headings
+        "." nil
+        :localleader
+        ;; SRC CODE
+         ". e"   'org-edit-src-code
+        ". E"   'org-babel-execute-src-block
+        ;; Links
+        ". d"   'org-toggle-link-display
+        ". o"   '+jg-org-open_link_in_buffer
+        ". O"   '+jg-org-open_link_externally
+        ". n"   '+jg-org-change_link_name
+        ;;Formatting
+        "i t"   '+jg-org-insert-heading-trio
+        ;; Citation
+        "i c" 'org-reftex-citation
+        )
+(map! :after bibtex-mode
+      :mode bibtex-mode
+      :localleader
+      "p" #'+jg-org-ref-open-bibtex-pdf)
--- modules/main/jg-org/config.el
+++ modules/main/jg-org/config.el
@@ -8,14 +8,10 @@
 (load! "+org-funcs")
 (load! "+org-pomodoro-funcs")
 (load! "+org-ref-funcs")
+(load! "+bindings")
 
 (use-package! academic-phrases
-  :init
-  (map! :leader
-        :prefix ("xA" . "Academic Phrases")
-        "p" #'academic-phrases
-        "s" #'academic-phrases-by-section
-        )
+  :defer t
 )
 (after! helm-org
     ;; TODO add a keybind for helm-org-rifle
@@ -36,67 +32,10 @@
    )
 
   (push 'org-indent-mode minor-mode-list)
-
-  (map! :map org-mode-map
-        "C-c [" nil
-        "C-c ]" nil
-        )
-
   (setq jg-org-map (make-sparse-keymap))
-
-  (map! :mode org-mode
-        :n "g j" #'org-forward-heading-same-level
-        :n "g k" #'org-backward-heading-same-level
-        :n "] p" #'org-next-link
-        :n "[ p" #'org-previous-link
-        :n  "g h" #'helm-org-in-buffer-headings
-        )
-
-  ;; add in keybinding to call tag-occurances
-  (map! :leader
-        ;; AGENDA
-        :prefix ("ao" . "Org")
-        (:prefix ("a" . "Agenda")
-         "/"   'org-occur-in-agenda-files
-         "f"   'org-agenda-file-to-front
-         "r"   'org-remove-file
-         "l"   'org-agenda-list
-         "F"   '+jg-org-list-agenda-files
-         "t"   'org-tags-view
-         )
-         ;; Agenda -> Calendar
-         )
-  (map! :leader
-        (:prefix "i"
-         "t" #'org-time-stamp)
-        (:prefix "j"
-         "c" #'org-goto-calendar)
-        )
-  ; TODO
-  (map! :mode org-mode
-        "." nil
-        ;; SRC CODE
-        ". e"   'org-edit-src-code
-        ". E"   'org-babel-execute-src-block
-        ;; Links
-        ". d"   'org-toggle-link-display
-        ". o"   '+jg-org-open_link_in_buffer
-        ". O"   '+jg-org-open_link_externally
-        ". n"   '+jg-org-change_link_name
-        ;;Formatting
-        "i t"   '+jg-org-insert-heading-trio
-        ;; Citation
-        "i c" 'org-reftex-citation
-        )
-
-  (map! :map org-agenda-mode-map
-        "v w"   'org-agenda-week-view
-        "v m"   'org-agenda-month-view
-        )
-
+  
   )
 (after! org-ref
-  (map! :leader "a U r" #'+jg-org-bibtex-load-random)
   (add-hook 'bibtex-mode-hook #'(lambda ()
                                 (map! :mode bibtex-mode
                                       :localleader

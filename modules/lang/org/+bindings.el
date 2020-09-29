1101c5dfeab5e6d61d93fd1fa68abec185d0874e
John Grey <johngrey4296@gmail.com>
2020-09-29 23:43:46 +0100

    adjust org bindings

--- modules/lang/org/+bindings.el
+++ modules/lang/org/+bindings.el
@@ -1,4 +1,221 @@
 ;;; lang/org/+bindings.el -*- lexical-binding: t; -*-
+;;; Misc
+;; (map! :map jg-org-mode-map
+;;       ;; textmate-esque newline insertion
+;;       "S-RET"      #'+org/shift-return
+;;       "C-RET"      #'+org/insert-item-below
+;;       "C-S-RET"    #'+org/insert-item-above
+;;       "C-M-RET"    #'org-insert-subheading
+;;       [C-return]   #'+org/insert-item-below
+;;       [C-S-return] #'+org/insert-item-above
+;;       [C-M-return] #'org-insert-subheading
+;;       (:when IS-MAC
+;;        [s-return]   #'+org/insert-item-below
+;;        [s-S-return] #'+org/insert-item-above
+;;        [s-M-return] #'org-insert-subheading)
+;;       ;; Org-aware C-a/C-e
+;;       [remap doom/backward-to-bol-or-indent]          #'org-beginning-of-line
+;;       [remap doom/forward-to-last-non-comment-or-eol] #'org-end-of-line
+
+;;       )
+;; ;;; Misc 2
+;; (map! :map jg-org-mode-map
+;;       :localleader
+;;       "#" #'org-update-statistics-cookies
+;;       "'" #'org-edit-special
+;;       "*" #'org-ctrl-c-star
+;;       "," #'org-switchb
+;;       "." #'org-goto
+;;       (:when (featurep! :completion ivy)
+;;        "." #'counsel-org-goto
+;;        "/" #'counsel-org-goto-all)
+;;       (:when (featurep! :completion helm)
+;;        "." #'helm-org-in-buffer-headings
+;;        "/" #'helm-org-agenda-files-headings)
+;;       )
+
+;; ;;; No Prefix
+;; (map! :map jg-org-mode-map
+;;       "A" #'org-archive-subtree
+;;       "e" #'org-export-dispatch
+;;       "f" #'org-footnote-new
+;;       "h" #'org-toggle-heading
+;;       "i" #'org-toggle-item
+;;       "I" #'org-toggle-inline-images
+;;       "n" #'org-store-link
+;;       "o" #'org-set-property
+;;       "p" #'org-priority
+;;       "q" #'org-set-tags-command
+;;       "t" #'org-todo
+;;       "T" #'org-todo-list
+;;       "x" #'org-toggle-checkbox
+
+;;       )
+;; ;;; <A>
+;; (map! :map jg-org-mode-map
+;;       :prefix ("a" . "attachments")
+;;        "a" #'org-attach
+;;        "d" #'org-attach-delete-one
+;;        "D" #'org-attach-delete-all
+;;        "f" #'+org/find-file-in-attachments
+;;        "l" #'+org/attach-file-and-insert-link
+;;        "n" #'org-attach-new
+;;        "o" #'org-attach-open
+;;        "O" #'org-attach-open-in-emacs
+;;        "r" #'org-attach-reveal
+;;        "R" #'org-attach-reveal-in-emacs
+;;        "u" #'org-attach-url
+;;        "s" #'org-attach-set-directory
+;;        "S" #'org-attach-sync
+;;        (:when (featurep! +dragndrop)
+;;         "c" #'org-download-screenshot
+;;         "p" #'org-download-clipboard
+;;         "P" #'org-download-yank)
+;;       )
+
+;; ;;; <B>
+;; (map! :map jg-org-mode-map
+;;       :prefix ("b" . "tables")
+;;        "-" #'org-table-insert-hline
+;;        "a" #'org-table-align
+;;        "b" #'org-table-blank-field
+;;        "c" #'org-table-create-or-convert-from-region
+;;        "e" #'org-table-edit-field
+;;        "f" #'org-table-edit-formulas
+;;        "h" #'org-table-field-info
+;;        "s" #'org-table-sort-lines
+;;        "r" #'org-table-recalculate
+;;        "R" #'org-table-recalculate-buffer-tables
+;;        (:prefix ("d" . "delete")
+;;         "c" #'org-table-delete-column
+;;         "r" #'org-table-kill-row)
+;;        (:prefix ("i" . "insert")
+;;         "c" #'org-table-insert-column
+;;         "h" #'org-table-insert-hline
+;;         "r" #'org-table-insert-row
+;;         "H" #'org-table-hline-and-move)
+;;        (:prefix ("t" . "toggle")
+;;         "f" #'org-table-toggle-formula-debugger
+;;         "o" #'org-table-toggle-coordinate-overlays)
+;;        (:when (featurep! +gnuplot)
+;;         "p" #'org-plot/gnuplot)
+;;       )
+;; ;;; <C>
+;; (map! :map jg-org-mode-map
+;;       (:prefix ("c" . "clock")
+;;        "c" #'org-clock-cancel
+;;        "d" #'org-clock-mark-default-task
+;;        "e" #'org-clock-modify-effort-estimate
+;;        "E" #'org-set-effort
+;;        "g" #'org-clock-goto
+;;        "G" (cmd! (org-clock-goto 'select))
+;;        "l" #'+org/toggle-last-clock
+;;        "i" #'org-clock-in
+;;        "I" #'org-clock-in-last
+;;        "o" #'org-clock-out
+;;        "r" #'org-resolve-clocks
+;;        "R" #'org-clock-report
+;;        "t" #'org-evaluate-time-range
+;;        "=" #'org-clock-timestamps-up
+;;        "-" #'org-clock-timestamps-down)
+;;       )
+;; ;;; <D>
+;; (map! :map jg-org-mode-map
+;;       (:prefix ("d" . "date/deadline")
+;;        "d" #'org-deadline
+;;        "s" #'org-schedule
+;;        "t" #'org-time-stamp
+;;        "T" #'org-time-stamp-inactive)
+;;       )
+;; ;;; <G>
+;; (map! :map jg-org-mode-map
+;;       (:prefix ("g" . "goto")
+;;        "g" #'org-goto
+;;        (:when (featurep! :completion ivy)
+;;         "g" #'counsel-org-goto
+;;         "G" #'counsel-org-goto-all)
+;;        (:when (featurep! :completion helm)
+;;         "g" #'helm-org-in-buffer-headings
+;;         "G" #'helm-org-agenda-files-headings)
+;;        "c" #'org-clock-goto
+;;        "C" (cmd! (org-clock-goto 'select))
+;;        "i" #'org-id-goto
+;;        "r" #'org-refile-goto-last-stored
+;;        "v" #'+org/goto-visible
+;;        "x" #'org-capture-goto-last-stored)
+;;       )
+;; ;;; <L>
+;; (map! :map jg-org-mode-map
+;;       (:prefix ("l" . "links")
+;;        "c" #'org-cliplink
+;;        "d" #'+org/remove-link
+;;        "i" #'org-id-store-link
+;;        "l" #'org-insert-link
+;;        "L" #'org-insert-all-links
+;;        "s" #'org-store-link
+;;        "S" #'org-insert-last-stored-link
+;;        "t" #'org-toggle-link-display)
+
+;;       )
+;; ;;; <P>
+;; (map! :map jg-org-mode-map
+;;       (:prefix ("P" . "publish")
+;;        "a" #'org-publish-all
+;;        "f" #'org-publish-current-file
+;;        "p" #'org-publish
+;;        "P" #'org-publish-current-project
+;;        "s" #'org-publish-sitemap)
+;;       )
+;; ;;; <R>
+;; (map! :map jg-org-mode-map
+;;       (:prefix ("r" . "refile")
+;;        "." #'+org/refile-to-current-file
+;;        "c" #'+org/refile-to-running-clock
+;;        "l" #'+org/refile-to-last-location
+;;        "f" #'+org/refile-to-file
+;;        "o" #'+org/refile-to-other-window
+;;        "O" #'+org/refile-to-other-buffer
+;;        "v" #'+org/refile-to-visible
+;;        "r" #'org-refile) ; to all `org-refile-targets'
+;;       )
+;; ;;; <S>
+;; (map! :map jg-org-mode-map
+;;       (:prefix ("s" . "Tree/Subtree")
+;;        "a" #'org-toggle-archive-tag
+;;        "b" #'org-tree-to-indirect-buffer
+;;        "d" #'org-cut-subtree
+;;        "h" #'org-promote-subtree
+;;        "j" #'org-move-subtree-down
+;;        "k" #'org-move-subtree-up
+;;        "l" #'org-demote-subtree
+;;        "n" #'org-narrow-to-subtree
+;;        "r" #'org-refile
+;;        "s" #'org-sparse-tree
+;;        "A" #'org-archive-subtree
+;;        "N" #'widen
+;;        "S" #'org-sort
+;;        (:prefix ("p" . "Org Priority")
+;;         "d" #'org-priority-down
+;;         "p" #'org-priority
+;;         "u" #'org-priority-up)))
+;;; Agenda
+(map! :after org-agenda
+      :map org-agenda-mode-map
+      :m "C-SPC" #'org-agenda-show-and-scroll-up
+      :localleader
+      (:prefix ("d" . "date/deadline")
+       "d" #'org-agenda-deadline
+       "s" #'org-agenda-schedule)
+      (:prefix ("c" . "clock")
+       "c" #'org-agenda-clock-cancel
+       "g" #'org-agenda-clock-goto
+       "i" #'org-agenda-clock-in
+       "o" #'org-agenda-clock-out
+       "r" #'org-agenda-clockreport-mode
+       "s" #'org-agenda-show-clocking-issues)
+      "q" #'org-agenda-set-tags
+      "r" #'org-agenda-refile
+      "t" #'org-agenda-todo)
 
 
 (map! :map evil-org-mode-map
--- modules/main/jg-org/+bindings.el
+++ modules/main/jg-org/+bindings.el
@@ -1,30 +1,33 @@
 ;;; main/jg-org/+bindings.el -*- lexical-binding: t; -*-
 
-(map! :leader
-        (:prefix ("xA" . "Academic Phrases")
-        "p" #'academic-phrases
-        "s" #'academic-phrases-by-section)
-        ;; AGENDA
-        (:prefix ("ao" . "Org")
-         (:prefix ("a" . "Agenda")
-          "/"   'org-occur-in-agenda-files
-          "f"   'org-agenda-file-to-front
-          "r"   'org-remove-file
-          "l"   'org-agenda-list
-          "F"   '+jg-org-list-agenda-files
-          "t"   'org-tags-view)
-         )
-        (:prefix "i"
-         "t" #'org-time-stamp)
-        (:prefix "j"
-         "c" #'org-goto-calendar)
-        "a U r" #'+jg-org-bibtex-load-random)
 
+;;; Academic phrases
+(map! :leader
+      (:prefix ("xA" . "Academic Phrases")
+       "p" #'academic-phrases
+       "s" #'academic-phrases-by-section)
+      ;; AGENDA
+      (:prefix ("ao" . "Org")
+       (:prefix ("a" . "Agenda")
+        "/"   'org-occur-in-agenda-files
+        "f"   'org-agenda-file-to-front
+        "r"   'org-remove-file
+        "l"   'org-agenda-list
+        "F"   '+jg-org-list-agenda-files
+        "t"   'org-tags-view)
+       )
+      (:prefix "i"
+       "t" #'org-time-stamp)
+      (:prefix "j"
+       "c" #'org-goto-calendar)
+      "a U r" #'+jg-org-bibtex-load-random)
+
+;;; Paren Control
 (map! :after org
       :map org-mode-map
-        "C-c [" nil
-        "C-c ]" nil
-        )
+      "C-c [" nil
+      "C-c ]" nil
+      )
 ;; (map! :after org
 ;;       :map org-agenda-mode-map
 ;; 	:prefix "v"
@@ -32,24 +35,34 @@
 ;;         "m"   #'org-agenda-month-view
 ;;         )
 
-(map! :after org
-      :mode org-mode
-        :n "g j" #'org-forward-heading-same-level
-        :n "g k" #'org-backward-heading-same-level
-        :n "] p" #'org-next-link
-        :n "[ p" #'org-previous-link
-        :n  "g h" #'helm-org-in-buffer-headings
-        "." nil
-        :localleader
-        ;; SRC CODE
-        ". e"   'org-edit-src-code
-        ". E"   'org-babel-execute-src-block
+;;; Personal
+(map! :map org-mode-map
+      (:prefix "g"
+       :n "j" #'org-forward-heading-same-level
+       :n "k" #'org-backward-heading-same-level)
+      (:prefix "]"
+       :n "p" #'org-next-link)
+      (:prefix "["
+       :n "[ p" #'org-previous-link)
+      (:prefix "g"
+       :n  "h" #'helm-org-in-buffer-headings)
+      "." nil
+      (:localleader
+       :n "." nil
+       :n "i" nil
+       ;; SRC CODE
+       (:prefix "."
+        :n "e"   'org-edit-src-code
+        :n "E"   'org-babel-execute-src-block
         ;; Links
-        ". d"   'org-toggle-link-display
-        ". o"   '+jg-org-open_link_in_buffer
-        ". O"   '+jg-org-open_link_externally
-        ". n"   '+jg-org-change_link_name
-        ;;Formatting
-        "i t"   '+jg-org-insert-heading-trio
-
+        :n "d"   'org-toggle-link-display
+        :n "o"   '+jg-org-open_link_in_buffer
+        :n "O"   '+jg-org-open_link_externally
+        :n "n"   '+jg-org-change_link_name
         )
+       ;;Formatting
+       :prefix "i"
+       :n "t"   '+jg-org-insert-heading-trio
+       )
+      )
+

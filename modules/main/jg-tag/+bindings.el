295e293b83d0b6fb8d50e4e95e8a8918afc15620
John Grey <johngrey4296@gmail.com>
2020-09-29 23:44:48 +0100

    adjust tagging bindings. see note for macro explanation
    map! seems to only work some of the time, so i've expanded the macro,
    and it seems to work ok now.

--- modules/main/jg-tag/+bindings.el
+++ modules/main/jg-tag/+bindings.el
@@ -17,12 +17,10 @@
 ;;                )
 ;;  )
 ;;
-(map! :after tag-clean-minor-mode
-      :map 'tag-clean-minor-mode-map
+(map! :map tag-clean-minor-mode-map
       "." #'tag-clean/body
       )
-(map! :after helm
-      :leader
+(map! :leader
       (:prefix ("ah" . "Helms")
        "f" #'jg-tag-helm-bookmarks
        "t" #'jg-tag-helm-twitter
@@ -31,30 +29,67 @@
        "b" #'jg-tag-helm-bibtex
        )
       )
-(map! :after org
-      :mode org-mode
-      :leader
-      (:prefix "ao"
-       :n "c" #'jg-tag-clean-org
-       :n "w" #'jg-tag-wrap-numbers
-       :n "L" #'jg-tag-wrap-non-link-urls
-       :n "D" #'jg-tag-remove-duplicates
-       :n "x s" #'jg-tag-next-similar-string
-       )
-      )
+
+;; (map! :leader
+;;       (:prefix ("ao" . "Org")
+;;        :n "c" #'jg-tag-clean-org
+;;        :n "w" #'jg-tag-wrap-numbers
+;;        :n "L" #'jg-tag-wrap-non-link-urls
+;;        :n "D" #'jg-tag-remove-duplicates
+;;        (:prefix "x"
+;;        :n "s" #'jg-tag-next-similar-string)
+;;        )
+;;       )
+
+(progn
+  (progn
+    (doom--define-leader-key
+     :states 'normal
+     :infix "ao"
+     "c" #'jg-tag-clean-org
+     "w" #'jg-tag-wrap-numbers
+     "L" #'jg-tag-wrap-non-link-urls
+     "D" #'jg-tag-remove-duplicates)
+    (doom--define-leader-key
+     :infix "ao" "" (list
+                     :ignore t
+                     :which-key "Org")))
+  (doom--define-leader-key
+   :states 'normal
+   :infix (general--concat nil "ao" "x")
+   "s" #'jg-tag-next-similar-string))
+
+
+
+
+
 (map! :leader
       "x l s" #'jg-tag-split-on-char-n
 )
 
-(map! :after bibtex
-      :mode bibtex-mode
-      :localleader
-      "." #'org-ref-bibtex-hydra/body
-      ;; Citation
-      ;; TODO : put this in org-mode bindings? : "i " 'org-reftex-citation
-      (:prefix ("o". "Open")
-       "p" #'+jg-org-ref-open-bibtex-pdf
-       ;; TODO add open bibtex dir...
-       )
+;; (map! :map bibtex-mode-map
+;;       :localleader
+;;       :n "." #'jg-org-ref-bibtex-hydra/body
+;;       :n "n" #'org-ref-bibtex-new-entry/body
+;;       ;; Citation
+;;       ;; TODO : put this in org-mode bindings? : "i " 'org-reftex-citation
+;;       :n "p" #'+jg-org-ref-open-bibtex-pdf
+;;       ;; TODO add open bibtex dir...
+;; )
 
+;; (define-localleader-key! :states 'normal :keymaps
+;;   '(bibtex-mode-map)
+;;   "." #'jg-org-ref-bibtex-hydra/body
+;;   "n" #'org-ref-bibtex-new-entry/body
+;;   "p" #'+jg-org-ref-open-bibtex-pdf)
+
+(general-define-key :states '(normal visual motion)
+                    :major-modes t
+                    :prefix doom-localleader-key
+                    :states 'normal
+                    :keymaps '(bibtex-mode-map)
+                    "." #'jg-org-ref-bibtex-hydra/body
+                    "n" #'org-ref-bibtex-new-entry/body
+                    "p" #'+jg-org-ref-open-bibtex-pdf
+                    "s" #'+jg-org-ref-search-scholar
 )

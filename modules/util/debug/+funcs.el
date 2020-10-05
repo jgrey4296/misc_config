1376c1e7c0660731eff89f0747f9cecb6001daf2
John Grey <johngrey4296@gmail.com>
2020-10-05 16:46:31 +0100

    add a misc util functions module

---
+++ modules/util/debug/+funcs.el
@@ -0,0 +1,6 @@
+;;; util/debug/+funcs.el -*- lexical-binding: t; -*-
+
+(defun debug-package-load (name)
+  (require 'loadhist)
+  (message "Package %s used in: %s" name (file-dependents (feature-file (intern name))
+  )

15cd8f0733a3ea8addf2a65e277f72f5d7096104
John Grey <johngrey4296@gmail.com>
2020-11-01 22:53:37 +0000

    disable evil-snipe in ibuffer

--- modules/editor/jg-personal/config.el
+++ modules/editor/jg-personal/config.el
@@ -98,6 +98,7 @@
   )
 (after! ibuffer
   (add-transient-hook! 'ibuffer-hook '+jg-personal-setup-ibuffer)
+  (push 'ibuffer-mode evil-snipe-disabled-modes)
   )
 (after! neotree
   (push "^__pycache__$" neo-hidden-regexp-list)

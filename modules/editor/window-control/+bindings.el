535a7c6adedac27091a664e785a8b7a44c70e801
John Grey <johngrey4296@gmail.com>
2021-02-27 15:46:36 +0000

    fix window-control binding descriptions

--- modules/editor/window-control/+bindings.el
+++ modules/editor/window-control/+bindings.el
@@ -3,28 +3,35 @@
 ;; Bindings
 (map! :leader
       (:prefix ("w r" . "Ring")
-        :n :desc "Pop Buffer (Alt: Pop to here)" "c" #'window-ring-pop-buffer
-        :n :desc "Clear Ring" "C" #'window-ring-clear-ring
+        :desc "Pop Buffer (Alt: Pop to here)" :n "c" #'window-ring-pop-buffer
+        :desc "Clear Ring" :n "C" #'window-ring-clear-ring
 
-        :n :desc "Ring Right" "l" #'window-ring-move-perspective
-        :n :desc "Ring Left" "h" #'window-ring-move-perspective-2
-        :n :desc "Most Recent" "L" #'window-ring-goto-most-recent
-        :n :desc "Oldest" "H" #'window-ring-goto-oldest
+        :desc "Ring Right" :n "l" #'window-ring-move-perspective
+        :desc "Ring Left" :n "h" #'window-ring-move-perspective-2
+        :desc "Most Recent" :n "L" #'window-ring-goto-most-recent
+        :desc "Oldest" :n "H" #'window-ring-goto-oldest
 
-        :n :desc "Add Current Buffer" "b" #'window-ring-add-current-buffer
-        :n :desc "Find File -> Head" "f" #'window-ring-add-to-head
-        :n :desc "Find File -> Tail" "F" #'window-ring-add-to-tail
+        :desc "Add Current Buffer" :n "b" #'window-ring-add-current-buffer
+        :desc "Find File -> Head" :n "f" #'window-ring-add-to-head
+        :desc "Find File -> Tail" :n "F" #'window-ring-add-to-tail
 
-        :n :desc "Print Sequence" "p" #'window-ring-print-order
+        :desc "Print Sequence" :n "p" #'window-ring-print-order
 
-        :n :desc "Window Ring soft Reset" "s" #'+window-ring-block-reset
-        :n :desc "Window Ring Hard Reset" "S" #'window-ring-setup-columns-command
-        :n :desc "Remove Current Buffer" "R" #'window-ring-remove-buffer
-        :n :desc "Replace with Buffer" "r" #'window-ring-replace-buffer
+        :desc "Window Ring soft Reset" :n "s" #'+window-ring-block-reset
+        :desc "Window Ring Hard Reset" :n "S" #'window-ring-setup-columns-command
+        :desc "Remove Current Buffer" :n "R" #'window-ring-remove-buffer
+        :desc "Replace with Buffer" :n "r" #'window-ring-replace-buffer
 
-        :n :desc "Toggle Ring Loop" "q" #'(lambda () (interactive) (setq window-ring-can-loop (not window-ring-can-loop)))
+        :desc "Toggle Ring Loop" :n "q" #'(lambda () (interactive) (setq window-ring-can-loop (not window-ring-can-loop)))
 
-        :n :desc "Shrink Side Wndows" "{" #'window-ring-shrink-sides
+        :desc "Shrink Side Wndows" :n "{" #'window-ring-shrink-sides
         )
       )
 
+(map! (:prefix "["
+       :desc "Ring Left" :n "r" #'window-ring-move-perspective-2
+       )
+      (:prefix "]"
+       :desc "Ring Right" :n "r" #'window-ring-move-perspective
+       )
+      )

cd2a34fc4472879978b11f60b562c0f044c53405
John Grey <johngrey4296@gmail.com>
2021-02-27 15:52:45 +0000

    fix uniquify binding description

--- modules/util/jg-misc/+bindings.el
+++ modules/util/jg-misc/+bindings.el
@@ -1,5 +1,6 @@
 ;;; util/jg-misc/+bindings.el -*- lexical-binding: t; -*-
 
 (map! :leader
-      (:prefix "x"
-      :n :desc "Uniqify" "l u" #'+jg-misc-uniquify))
+      :prefix "x"
+      :desc "Uniqify" :n "l u" #'+jg-misc-uniquify
+      )

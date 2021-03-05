1e4f35205a85ef200e307a5f73d83b474a17ad19
John Grey <johngrey4296@gmail.com>
2021-03-05 16:53:55 +0000

    fix typo

--- modules/domain-specific/music/packages.el
+++ modules/domain-specific/music/packages.el
@@ -1,6 +1,6 @@
 ;; sclang packages.el
 ;; loads second
 (package! sclang                    :recipe (:local-repo "~/.doom.d/packages/sclang"))
-(package! tidal                     :recipe (:local-repo "~/.doom.d/pakages/tidal"))
+(package! tidal                     :recipe (:local-repo "~/.doom.d/packages/tidal"))
 (package! chuck-mode                :recipe (:local-repo "~/.doom.d/packages/chuck-mode"))
 (package! music-minor-mode :recipe (:local-repo "~/.doom.d/packages/music-minor-mode"))

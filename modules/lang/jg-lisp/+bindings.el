15dee40a339e45a616156972de0564ea0e1fb528
John Grey <johngrey4296@gmail.com>
2021-03-25 18:44:32 +0000

    implement basic defun sorting

--- modules/lang/jg-lisp/+bindings.el
+++ modules/lang/jg-lisp/+bindings.el
@@ -1,1 +1,6 @@
 ;;; lang/emacs-lisp/+bindings.el -*- lexical-binding: t; -*-
+
+(map! :map emacs-lisp-mode-map
+      :localleader
+      :desc "Sort Defuns" "S" #'+jg-lisp-sort-defuns
+      )
--- modules/lang/jg-lisp/+funcs.el
+++ modules/lang/jg-lisp/+funcs.el
@@ -1,4 +1,21 @@
-;;; lang/emacs-lisp/+funcs.el -*- lexical-binding: t; -*-
+;;; jg-list/+funcs.el --- summary -*- lexical-binding: t -*-
 
+;; Simple Functions to feed into sort-subr
+(defun +__jg-lisp-key-start ()
+  (re-search-forward "(defun " nil t)
+  (symbol-at-point))
+(defun +__jg-lisp-next-rec-end-func ()
+  (evil-forward-end 'evil-defun))
+(defun +__jg-lisp-next-rec-func ()
+  (evil-forward-beginning 'evil-defun))
 
-;; TODO add defun sorting
+;; Run this
+(defun +jg-lisp-sort-defuns ()
+  (interactive)
+  (goto-char (point-min))
+  (sort-subr nil
+             #'+__jg-lisp-next-rec-func
+             #'+__jg-lisp-next-rec-end-func
+             #'+__jg-lisp-key-start)
+  (goto-char (point-min))
+  )

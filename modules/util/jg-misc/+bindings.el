5ad8ac44af47cfff69543c0d4261f0dd012fb4eb
John Grey <johngrey4296@gmail.com>
2021-03-26 22:55:20 +0000

    move free-keys into help-map

--- modules/util/jg-misc/+bindings.el
+++ modules/util/jg-misc/+bindings.el
@@ -1,14 +1,14 @@
 ;;; util/jg-misc/+bindings.el -*- lexical-binding: t; -*-
 
- (map! :leader
-       (:prefix "b"
-        :desc "Undo-Tree" "u" #'+jg-misc-undo-tree)
-       (:prefix "t"
-        "v r" #'rainbow-mode)
-       (:prefix "h"
-         "DEL" #'free-keys
-        )
-       )
+(map! :leader
+      (:prefix "b"
+       :desc "Undo-Tree" "u" #'+jg-misc-undo-tree)
+      (:prefix "t"
+       "v r" #'rainbow-mode)
+      )
+(map! :map help-map
+       "DEL" #'free-keys
+      )
 
 (map! :map free-keys-mode-map
       :desc "Change Buffer" :n "b" #'free-keys-change-buffer

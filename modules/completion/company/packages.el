c7ab9aecaac98948a292e3c8350caa6996cafa05
John Grey <johngrey4296@gmail.com>
2021-04-02 15:43:20 +0100

    remove obsolete files

--- modules/completion/company/packages.el
+++
@@ -1,8 +0,0 @@
-;; -*- no-byte-compile: t; -*-
-;;; completion/company/packages.el
-
-(package! cl-lib)
-(package! company )
-(package! company-dict )
-(when (featurep! +childframe)
-  (package! company-box ))

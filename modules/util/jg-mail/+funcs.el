9d5c7517a68c68420eebd8e9cf0b8cf8c8a2b288
John Grey <johngrey4296@gmail.com>
2021-04-17 18:48:30 +0100

    set mu4e customisations in hook, so after use-package config

---
+++ modules/util/jg-mail/+funcs.el
@@ -0,0 +1,32 @@
+;;; util/jg-mail/+funcs.el -*- lexical-binding: t; -*-
+
+(defun +jg-mail-override-mu4e-hook ()
+  (setq-default smtpmail-smtp-service 465
+                message-send-mail-function   'smtpmail-send-it
+                smtpmail-default-smtp-server "smtp.gmail.com"
+                smtpmail-smtp-server         "smtp.gmail.com"
+                smtpmail-stream-type  'starttls
+                smtpmail-smtp-service 587
+                auth-sources '("~/.authinfo"
+                               macos-keychain-generic
+                               macos-keychain-internet
+                               "~/authinfo.gpg")
+                )
+
+  (setq mu4e-use-fancy-chars nil
+        mu4e-headers-draft-mark     '("D" . "D")
+        mu4e-headers-flagged-mark   '("F" . "F")
+        mu4e-headers-new-mark       '("N" . "N")
+        mu4e-headers-passed-mark    '("P" . "P")
+        mu4e-headers-replied-mark   '("R" . "R")
+        mu4e-headers-seen-mark      '("S" . "S")
+        mu4e-headers-trashed-mark   '("T" . "T")
+        mu4e-headers-attach-mark    '("a" . "a")
+        mu4e-headers-encrypted-mark '("x" . "x")
+        mu4e-headers-signed-mark    '("s" . "s")
+        mu4e-headers-unread-mark    '("u" . "u"))
+
+  (setq auth-source-backend-parser-functions
+        '(auth-source-backends-parser-secrets
+          auth-source-backends-parser-file))
+)
--- modules/util/jg-mail/config.el
+++ modules/util/jg-mail/config.el
@@ -4,6 +4,7 @@
 (after! evil
   (load! "+bindings")
   )
+(load! "+funcs")
 
 (set-email-account! "jgrey4296"
                     '((mu4e-sent-folder       . "/[Gmail].Sent Mail")
@@ -15,33 +16,6 @@
                       (mu4e-compose-signature . "---\nJohn"))
                     t)
 
-(after! mu4e
-  (setq-default smtpmail-smtp-service 465
-                message-send-mail-function   'smtpmail-send-it
-                smtpmail-default-smtp-server "smtp.gmail.com"
-                smtpmail-smtp-server         "smtp.gmail.com"
-                smtpmail-stream-type  'starttls
-                smtpmail-smtp-service 587
-                auth-sources '("~/.authinfo"
-                               macos-keychain-generic
-                               macos-keychain-internet
-                               "~/authinfo.gpg")
-                )
-
-  (setq mu4e-use-fancy-chars t
-        mu4e-headers-draft-mark     '("D" . "D")
-        mu4e-headers-flagged-mark   '("F" . "F")
-        mu4e-headers-new-mark       '("N" . "N")
-        mu4e-headers-passed-mark    '("P" . "P")
-        mu4e-headers-replied-mark   '("R" . "R")
-        mu4e-headers-seen-mark      '("S" . "S")
-        mu4e-headers-trashed-mark   '("T" . "T")
-        mu4e-headers-attach-mark    '("a" . "a")
-        mu4e-headers-encrypted-mark '("x" . "x")
-        mu4e-headers-signed-mark    '("s" . "s")
-        mu4e-headers-unread-mark    '("u" . "u"))
-
-  (setq auth-source-backend-parser-functions
-        '(auth-source-backends-parser-secrets
-          auth-source-backends-parser-file))
-)
+(use-package! mu4e
+  :hook (mu4e-main-mode . +jg-mail-override-mu4e-hook)
+  )

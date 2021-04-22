eed82ded5099e94077504228b073a9b1ed695ea0
John Grey <johngrey4296@gmail.com>
2021-04-22 22:09:58 +0100

    shift text functions into motions

--- modules/util/text/+funcs.el
+++ modules/util/text/+funcs.el
@@ -1,73 +1,12 @@
 ;;; util/text/+funcs.el -*- lexical-binding: t; -*-
-(defun +jg-text-strip-spaces (str)
-  "Utility to replace spaces with underscores in a string.
-Used to guard inputs in tag strings"
-  (s-replace " " "_" (string-trim str))
-  )
-(defun +jg-text-split-on-char-n (beg end n)
-  "Loop through buffer, inserting newlines between lines where
-the nth char changes"
-  (interactive "r\nnSplit on char number: ")
-  (save-excursion
-    (goto-char beg)
-    (let ((last-char (downcase (char-after (+ (point) n))))
-          curr-char)
-      (while (< (point) end)
-        (setq curr-char (downcase (char-after (+ (point) n))))
-        (if (not (eq last-char curr-char))
-            (progn
-              (setq last-char curr-char)
-              (goto-char (line-beginning-position))
-              (insert "\n")
-              )
-          )
-        (forward-line)
-        )
-      )
-    )
-  )
-(defun +jg-text-next-similar-string ()
-  " Go through lines, finding the next adjacent string pair
-uses org-babel-edit-distance "
-  (interactive)
-  (let* ((bound (or current-prefix-arg jg-text-last-similarity-arg))
-         (curr-sim (+ bound 1))
-         (s2 (downcase (string-trim (car (s-split ":" (buffer-substring (line-beginning-position) (line-end-position)))))))
-         s1
-         )
-    (while (and (< (point) (point-max))
-                (> curr-sim bound))
-      (forward-line)
-      (setq jg-text-last-similarity-arg bound)
-      (setq s1 s2)
-      (setq s2 (downcase (string-trim (car (s-split ":" (buffer-substring (line-beginning-position) (line-end-position)))))))
-      (setq curr-sim (org-babel-edit-distance s1 s2))
-      )
-    )
-  (message "Using distance %s" jg-text-last-similarity-arg)
-  )
 (defun +jg-get-line ()
   (buffer-substring-no-properties (line-beginning-position)
                                   (line-end-position))
   )
-(defun +jg-text-uniquify (L R)
-  (interactive "r")
-  (save-excursion
-    (goto-char L)
-    (let ((R-mark (set-marker (make-marker) R))
-          (current (+jg-get-line))
-          (kill-whole-line t))
-      (forward-line 1)
-      (while (<= (point) R-mark)
-        ;; compare
-        (if (s-equals? current (+jg-get-line))
-            (kill-line)
-          (progn (setq current (+jg-get-line))
-                 (forward-line 1))
-          )
-        )
-      )
-    )
+(defun +jg-text-strip-spaces (str)
+  "Utility to replace spaces with underscores in a string.
+Used to guard inputs in tag strings"
+  (s-replace " " "_" (string-trim str))
   )
 
 (defun +jg-text-regex-reminder ()
@@ -77,22 +16,6 @@ uses org-babel-edit-distance "
     (princ (yas--template-content (yas-lookup-snippet "Char Classes" 'fundamental-mode)))
     )
   nil
-  )
-
-(defun +jg-text-simple-grep (reg)
-  " Copy any matching line into a separate buffer "
-  (interactive "sMatch Regexp: ")
-  (with-temp-buffer-window "*Text-Results*" 'display-buffer-pop-up-window nil
-    (goto-char (point-min))
-    (while (re-search-forward reg nil t)
-      (princ (format "%s : %s\n" (line-number-at-pos) (buffer-substring (line-beginning-position) (line-end-position))))
-      )
-    )
-  (let ((inhibit-read-only t))
-    (with-current-buffer "*Text-Results*"
-      (align-regexp (point-min) (point-max) "\\(\s-*\\):")
-      )
-    )
   )
 
 (defun +jg-text-remove-leading-whitespace ()
@@ -110,45 +33,119 @@ uses org-babel-edit-distance "
     )
   )
 
-(defun +jg-text-title-case ()
-  (interactive)
-  (let ((start (if (evil-visual-state-p) evil-visual-beginning (line-beginning-position)))
-        (end (if (evil-visual-state-p) evil-visual-end (line-end-position))))
-    (save-excursion
-      (goto-char start)
-      (while (< (point) end)
-        (capitalize-word)
-        )
-      )
-    )
-  )
+;; (defun +jg-text-title-case ()
+;;   (interactive)
+;;   (let ((start (if (evil-visual-state-p) evil-visual-beginning (line-beginning-position)))
+;;         (end (if (evil-visual-state-p) evil-visual-end (line-end-position))))
+;;     (save-excursion
+;;       (goto-char start)
+;;       (while (< (point) end)
+;;         (capitalize-word)
+;;         )
+;;       )
+;;     )
+;;   )
+;; (defun +jg-text-escalate-replace ()
+;;   " Replace a regex in the region,
+;; with either a numeric or alphabetical escalation "
+;;   (interactive)
+;;   (let* ((start (if (evil-visual-state-p) evil-visual-beginning (point-min)))
+;;          (end (set-marker (make-marker) (if (evil-visual-state-p) evil-visual-end (point-max))))
+;;          (reg (read-string "Regexp: " "^.*?\\( \\)"))
+;;          (base (read-string "Replacement Base: "))
+;;          (type (car (read-multiple-choice "Increment Digit or Char? "
+;;                                           '((?d "Digit")
+;;                                             (?c "Char")))))
+;;          (current (if (eq ?d type) 0 ?a))
+;;         )
 
-
-(defun +jg-text-escalate-replace ()
-  " Replace a regex in the region,
-with either a numeric or alphabetical escalation "
-  (interactive)
-  (let* ((start (if (evil-visual-state-p) evil-visual-beginning (point-min)))
-         (end (set-marker (make-marker) (if (evil-visual-state-p) evil-visual-end (point-max))))
-         (reg (read-string "Regexp: " "^.*?\\( \\)"))
-         (base (read-string "Replacement Base: "))
-         (type (car (read-multiple-choice "Increment Digit or Char? "
-                                          '((?d "Digit")
-                                            (?c "Char")))))
-         (current (if (eq ?d type) 0 ?a))
-        )
-
-    (save-excursion
-      (goto-char start)
-      (while (re-search-forward reg end t)
-        (replace-match (format "%s_%s" base (if (eq ?c type) (char-to-string current) current))
-                       t
-                       nil
-                       nil
-                       1)
-        (incf current)
-        (if (and (eq type ?c) (> current ?z)) (setq current ?a))
-        )
-      )
-    )
-  )
+;;     (save-excursion
+;;       (goto-char start)
+;;       (while (re-search-forward reg end t)
+;;         (replace-match (format "%s_%s" base (if (eq ?c type) (char-to-string current) current))
+;;                        t
+;;                        nil
+;;                        nil
+;;                        1)
+;;         (incf current)
+;;         (if (and (eq type ?c) (> current ?z)) (setq current ?a))
+;;         )
+;;       )
+;;     )
+;;   )
+;; (defun +jg-text-next-similar-string ()
+;;   " Go through lines, finding the next adjacent string pair
+;; uses org-babel-edit-distance "
+;;   (interactive)
+;;   (let* ((bound (or current-prefix-arg jg-text-last-similarity-arg))
+;;          (curr-sim (+ bound 1))
+;;          (s2 (downcase (string-trim (car (s-split ":" (buffer-substring (line-beginning-position) (line-end-position)))))))
+;;          s1
+;;          )
+;;     (while (and (< (point) (point-max))
+;;                 (> curr-sim bound))
+;;       (forward-line)
+;;       (setq jg-text-last-similarity-arg bound)
+;;       (setq s1 s2)
+;;       (setq s2 (downcase (string-trim (car (s-split ":" (buffer-substring (line-beginning-position) (line-end-position)))))))
+;;       (setq curr-sim (org-babel-edit-distance s1 s2))
+;;       )
+;;     )
+;;   (message "Using distance %s" jg-text-last-similarity-arg)
+;;   )
+;; (defun +jg-text-uniquify (L R)
+;;   (interactive "r")
+;;   (save-excursion
+;;     (goto-char L)
+;;     (let ((R-mark (set-marker (make-marker) R))
+;;           (current (+jg-get-line))
+;;           (kill-whole-line t))
+;;       (forward-line 1)
+;;       (while (<= (point) R-mark)
+;;         ;; compare
+;;         (if (s-equals? current (+jg-get-line))
+;;             (kill-line)
+;;           (progn (setq current (+jg-get-line))
+;;                  (forward-line 1))
+;;           )
+;;         )
+;;       )
+;;     )
+;;   )
+;; (defun +jg-text-split-on-char-n (beg end n)
+;;   "Loop through buffer, inserting newlines between lines where
+;; the nth char changes"
+;;   (interactive "r\nnSplit on char number: ")
+;;   (save-excursion
+;;     (goto-char beg)
+;;     (let ((last-char (downcase (char-after (+ (point) n))))
+;;           curr-char)
+;;       (while (< (point) end)
+;;         (setq curr-char (downcase (char-after (+ (point) n))))
+;;         (if (not (eq last-char curr-char))
+;;             (progn
+;;               (setq last-char curr-char)
+;;               (goto-char (line-beginning-position))
+;;               (insert "\n")
+;;               )
+;;           )
+;;         (forward-line)
+;;         )
+;;       )
+;;     )
+;;   )
+;; (defun +jg-text-simple-grep (reg)
+;;   " Copy any matching line into a separate buffer "
+;;   (interactive "sMatch Regexp: ")
+;;   (with-temp-buffer-window "*Text-Results*" 'display-buffer-pop-up-window nil
+;;     (goto-char (point-min))
+;;     (while (re-search-forward reg nil t)
+;;       (princ (format "%s : %s\n" (line-number-at-pos) (buffer-substring (line-beginning-position) (line-end-position))))
+;;       )
+;;     )
+;;   (let ((inhibit-read-only t))
+;;     (with-current-buffer "*Text-Results*"
+;;       (align-regexp (point-min) (point-max) "\\(\s-*\\):")
+;;       )
+;;     )
+;;   )
--- modules/util/text/+motions.el
+++ modules/util/text/+motions.el
@@ -5,12 +5,7 @@
 ;; Text Objects : A Motion Selection
 ;; Operators    : Act on text moved over by motion
 
-(evil-define-text-object +jg-text-line-object (count)
-  "For selecting the entire line"
-  :extend-selection t
-  (list (line-beginning-position) (line-end-position))
-  )
-
+;; Motions
 (evil-define-motion +jg-text-prev-close-paren-motion (count)
   "Go to [count] next unmatched ')'."
   :type exclusive
@@ -27,15 +22,32 @@
 (evil-define-motion +jg-text-next-empty-line-motion (count)
   :type exclusive
   (forward-line)
-  (re-search-forward "^[[:space:]]*$" nil nil count)
-
+  (re-search-forward "^[[:space:]]*$" nil nil (or count 1))
   )
 (evil-define-motion +jg-text-prev-empty-line-motion (count)
   :type exclusive
   (forward-line -1)
-  (re-search-backward "^[[:space:]]*$" nil nil count)
+  (re-search-backward "^[[:space:]]*$" nil nil (or  count 1))
   )
 
+(evil-define-motion +jg-text-next-similar-string (count)
+  (interactive)
+  (let* ((bound (or current-prefix-arg jg-text-last-similarity-arg))
+         (curr-sim (+ bound 1))
+         (s2 (downcase (string-trim (car (s-split ":" (buffer-substring (line-beginning-position) (line-end-position)))))))
+         s1)
+    (while (and (< (point) (point-max))
+                (> curr-sim bound))
+      (forward-line)
+      (setq jg-text-last-similarity-arg bound)
+      (setq s1 s2)
+      (setq s2 (downcase (string-trim (car (s-split ":" (buffer-substring (line-beginning-position) (line-end-position)))))))
+      (setq curr-sim (org-babel-edit-distance s1 s2))
+      )
+    )
+  )
+
+;; Text Objects
 (evil-define-text-object +jg-text-grow-selection-op (count)
   " Grow the selection on either side by count "
   :type exclusive
@@ -43,28 +55,109 @@
   (interactive)
   (list (- evil-visual-beginning 1) (+ evil-visual-end 1))
   )
-
 (evil-define-text-object +jg-text-line-textobj (count)
   " Line object of the current line "
   :type line
   (interactive)
   (list (line-beginning-position) (line-end-position))
   )
+(evil-define-text-object +jg-text-whole-buffer-textobj (count)
+  " Line object of the current line "
+  :type inclusive
+  (interactive)
+  (list (point-min) (point-max))
+  )
 
-(evil-define-operator +jg-text-split-on-char (beg end count)
-  ;; TODO
-
+;; Operators
+(evil-define-operator +jg-text-split-on-char (beg end)
+  :move-point t
+  (message "Count: %s" count)
+  (let ((last-char (downcase (char-after (+ (point) count))))
+        curr-char)
+    (while (< (point) end)
+      (setq curr-char (downcase (char-after (+ (point) count))))
+      (if (not (eq last-char curr-char))
+          (progn
+            (setq last-char curr-char)
+            (goto-char (line-beginning-position))
+            (insert "\n")
+            )
+        )
+      (forward-line)
+      )
+    )
   )
 (evil-define-operator +jg-text-remove-leading-whitespace-op (beg end count)
-  ;; TODO
+  :move-point t
+  (while (< (point) end)
+    (evil-first-non-blank)
+    (delete-region (line-beginning-position) (point))
+    (forward-line)
+    )
   )
 (evil-define-operator +jg-text-uniquify-op (beg end count)
-  ;; TODO
+  :move-point t
+  (evil-first-non-blank)
+  (let ((R-mark (set-marker (make-marker) end))
+        (current (+jg-get-line))
+        (kill-whole-line t))
+    (forward-line 1)
+    (while (<= (point) R-mark)
+      ;; compare
+      (if (s-equals? current (+jg-get-line))
+          (kill-line)
+        (progn (setq current (+jg-get-line))
+               (forward-line 1))
+        )
+      )
+    )
   )
+(evil-define-operator +jg-text-escalate-replace-op (beg end count)
+  " Replace a regex in the region,
+with either a numeric or alphabetical escalation "
+  :move-point t
+  (let* ((end-mark (set-marker (make-marker) end))
+         (reg  (read-string "Regexp: " "^.*?\\( \\)"))
+         (base (read-string "Replacement Base: "))
+         (type (car (read-multiple-choice "Increment Digit or Char? "
+                                          '((?d "Digit")
+                                            (?c "Char")))))
+         (current (if (eq ?d type) 0 ?a))
+         )
+    (goto-char beg)
+    (while (re-search-forward reg end-mark t)
+      (replace-match (format "%s_%s" base (if (eq ?c type) (char-to-string current) current))
+                     t nil nil 1)
+      (incf current)
+      (if (and (eq type ?c) (> current ?z)) (setq current ?a))
+      )
+    )
+  )
+(evil-define-operator +jg-text-title-case-op (beg end)
+  :move-point t
+  (while (< (point) end)
+    (capitalize-word 1)
+    )
+  )
+(evil-define-operator +jg-text-simple-grep-op (beg end count)
+  :move-point t
+  (interactive)
+  (let ((reg (read-string "Match Regexp: ")))
+    (with-temp-buffer-window "*Text-Results*" 'display-buffer-pop-up-window nil
+      (goto-char (point-min))
+      (while (re-search-forward reg end t)
+        (princ (format "%s : %s\n" (line-number-at-pos) (buffer-substring (line-beginning-position) (line-end-position))))
+        )
+      )
+    )
+  (let ((inhibit-read-only t))
+    (with-current-buffer "*Text-Results*"
+      (align-regexp (point-min) (point-max) "\\(\s-*\\):")
+      )
+    )
+  )
+
 (evil-define-operator +jg-text-line-on-char-op (beg end count)
   ;; TODO
 
-  )
-(evil-define-operator +jg-text-strip-spaces-op (beg end count)
- ;; TODO
   )

a87a320ff32db64b9677f833078e1340c668d61d
John Grey <johngrey4296@gmail.com>
2021-04-22 22:09:11 +0100

    add personal keymap vars

--- modules/emacs/bindings/+vars.el
+++ modules/emacs/bindings/+vars.el
@@ -3,4 +3,15 @@
 (setq-default jg-google-url "https://duckduckgo.com/?q=%s"
               jg-twitter-url "https://twitter.com"
               default-input-method "greek"
-              )
+)
+
+
+(setq jg-binding-operator-map           (make-sparse-keymap "evil operators")
+      jg-binding-vision-map             (make-sparse-keymap "vision manipulation")
+      jg-binding-forward-motion-map     (make-sparse-keymap "forward motion")
+      jg-binding-backward-motion-map    (make-sparse-keymap "backward motion")
+      jg-binding-inner-text-objects-map (make-sparse-keymap "inner textobjs")
+      jg-binding-outer-text-objects-map (make-sparse-keymap "outer textobjs"))
+
+
+

4b5bca7118d437856c14d7268a86574167d2bd8d
John Grey <johngrey4296@gmail.com>
2021-04-30 10:52:02 +0100

    adjust hooks

--- modules/emacs/bindings/config.el
+++ modules/emacs/bindings/config.el
@@ -2,13 +2,20 @@
 
 (load! "+funcs")
 (load! "+vars")
-(load! "+leader-bindings")
-(load! "+leaderless-bindings")
-(load! "+evil-ex-setup")
-(load! "+evil-bindings")
-(load! "+evil-bindings-2")
-(load! "+ibuffer-bindings")
 (load! "+which-key-update")
+
+(after! evil
+  (load! "+leader-bindings")
+  (load! "+leaderless-bindings")
+  (load! "+evil-bindings")
+  (load! "+evil-bindings-2")
+  (load! "+ibuffer-bindings")
+)
+(after! evil-ex
+  (load! "+evil-ex-setup")
+  (+jg-binding-evil-ex-setup-hook)
+  )
+
 
 (after! flycheck
   (map! :leader
@@ -21,13 +28,9 @@
 (add-hook 'doom-first-input-hook #'+jg-binding-setup-leaders-hook -100)
 (add-hook 'doom-first-input-hook #'+jg-binding-setup-leaderless-hook -100)
 (add-hook 'doom-first-input-hook #'+jg-binding-evil-total-hook 100)
+(add-hook 'doom-first-input-hook #'+jg-binding-evil-submap-hook)
 
 (add-hook! ibuffer-mode #'+jg-ibuffer-setup-hook)
 (add-hook! ibuffer-mode #'+jg-ibuffer-update-hook)
 
 
-(add-hook 'evil-after-load-hook #'+jg-binding-evil-submap-hook)
-
-(after! evil-ex
-  (+jg-binding-evil-ex-setup-hook)
-  )

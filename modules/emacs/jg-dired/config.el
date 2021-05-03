3ead8482000258df028de9d0a14aaacf81ceb83b
John Grey <johngrey4296@gmail.com>
2021-05-03 09:03:08 +0100

    customise dired mark line face

--- modules/emacs/jg-dired/config.el
+++ modules/emacs/jg-dired/config.el
@@ -10,6 +10,10 @@
   :commands hydra-dired-quick-sort/body
   )
 
+(use-package! diredfl
+  :config
+  (set-face-attribute 'diredfl-flag-mark-line nil :background "blueviolet")
+)
 
 (add-hook 'doom-first-input-hook #'+jg-dired-binding-hook)
 (add-hook 'dired-load-hook #'+jg-dired-var-hook)

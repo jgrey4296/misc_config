fe3019df97b1097b517f83e62e86b8c434f289e2
John Grey <johngrey4296@gmail.com>
2021-05-19 02:44:13 +0100

    fix location of total tags

--- modules/emacs/jg-tag/+vars.el
+++ modules/emacs/jg-tag/+vars.el
@@ -6,7 +6,7 @@
               jg-tag-alt-mapping (make-hash-table)
 
               jg-tag-loc-bookmarks             "~/github/writing/resources/main_bookmarks.html"
-              jg-tag-loc-global-tags           "~/github/writing/resources/collate.tags"
+              jg-tag-loc-global-tags           "~/github/writing/resources/cron_reports/totals.tags"
               jg-tag-loc-twitter-account-index "~/.doom.d/setup_files/tw_acct.index"
               jg-tag-loc-twitter-tag-index     "~/.doom.d/setup_files/tw_tag.index"
               jg-tag-loc-default-helm-directory "~/github/writing/resources/"

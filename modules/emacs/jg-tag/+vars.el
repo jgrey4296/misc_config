68f032c135de22d41c0773dc12c1cb57197cc21f
John Grey <johngrey4296@gmail.com>
2021-05-20 03:12:41 +0100

    update locations for helms

--- modules/emacs/jg-tag/+vars.el
+++ modules/emacs/jg-tag/+vars.el
@@ -5,10 +5,10 @@
               jg-tag-global-tags (make-hash-table :test 'equal)
               jg-tag-alt-mapping (make-hash-table)
 
-              jg-tag-loc-bookmarks             "~/github/writing/resources/main_bookmarks.html"
+              jg-tag-loc-bookmarks             "~/github/writing/resources/bookmarks/main_bookmarks.html"
               jg-tag-loc-global-tags           "~/github/writing/resources/cron_reports/totals.tags"
-              jg-tag-loc-twitter-account-index "~/.doom.d/setup_files/tw_acct.index"
-              jg-tag-loc-twitter-tag-index     "~/.doom.d/setup_files/tw_tag.index"
+              jg-tag-loc-twitter-account-index "~/github/writing/resources/cron_reports/tw_acct.index"
+              jg-tag-loc-twitter-tag-index     "~/github/writing/resources/cron_reports/tw_tag.index"
               jg-tag-loc-default-helm-directory "~/github/writing/resources/"
 
               jg-tag-marker (make-marker)

0ac10dc95cb443d95b9e380c6faee0e9763c610d
John Grey <johngrey4296@gmail.com>
2021-05-25 02:05:07 +0100

    add mode stubs

--- packages/acab-ide/acab-string-expansion-mode.el
+++ packages/acab-ide/acab-string-expansion-mode.el
@@ -1,3 +1,21 @@
 ;; -*- mode: elisp; lexical-binding: t; -*-
 
 ;; TODO a tracery-inspired string expansion helper for authoring grammars
+(provide 'acab-string-expansion-mode)
+
+(define-minor-mode acab-string-expansion-minor-mode
+  " Mode for manipulating string possibility grammars "
+  :lighter "acab-string-expansion"
+                                        ; :global t
+                                        ; :keymap nil
+
+  )
+
+(defun acab-string-expansion-minor-mode/turn-on ()
+  (unless (minibufferp)
+    (if (eq major-mode 'prog-mode)
+        (acab-string-expansion-minor-mode 1))
+    )
+  )
+
+(define-globalized-minor-mode global-acab-string-expansion-minor-mode acab-string-expansion-minor-mode/turn-on)

89209ad3484857e0c75f184696341fb4e6364cd5
John Grey <johngrey4296@gmail.com>
2021-05-25 02:18:22 +0100

    add company-gtags backend for python

--- modules/completion/jg-completion/config.el
+++ modules/completion/jg-completion/config.el
@@ -7,9 +7,14 @@
 (after! (ivy counsel)
   (load! "+ivy_actions")
   )
+
 (after! helm-mode
   (map! :map helm-map
         "<tab>" nil
         "TAB" #'helm-select-action
         )
+  )
+
+(after! (company gtags helm-gtags)
+  (set-company-backend! 'python-mode 'company-gtags)
   )

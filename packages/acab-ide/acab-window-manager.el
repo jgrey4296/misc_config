66c8d4c64b5d37e52236651e9ef304ae46c8d9e6
John Grey <johngrey4296@gmail.com>
2021-05-25 20:42:16 +0100

    move code into window manager

--- packages/acab-ide/acab-window-manager.el
+++ packages/acab-ide/acab-window-manager.el
@@ -26,13 +26,18 @@
               )
 
 (defun acab-wm/init ()
+  (window--display-buffer (find-file (f-join location acab-ide/ide-pipeline-spec-buffer)) (plist-get windows :miscL) 'window)
+  (window--display-buffer (generate-new-buffer "rule_stub")  (plist-get windows :rule) 'window)
+  (window--display-buffer (get-buffer-create acab-ide/inputs-buffer-name)  (plist-get windows :prior)'window)
+  (window--display-buffer (get-buffer-create acab-ide/outputs-buffer-name)  (plist-get windows :post) 'window)
+  (window--display-buffer (get-buffer-create acab-ide/logging-buffer-name)  (plist-get windows :miscR) 'window)
+  (window--display-buffer (get-buffer-create acab-ide/working-group-buffer-name)  (plist-get windows :miscC) 'window)
+
 
   )
-
 (defun acab-wm/reset ()
 
   )
-
 (defun acab-wm/popup ()
 
   )
@@ -106,4 +111,56 @@
           (insert (format "  %s\n" x)) ;;Maybe propertize
           )
     )
+  )
+
+(defun acab-ide/init-ide-buffers-contents (location inst-name windows)
+  ;;create inst stub
+  (if (not (f-exists? (f-join location acab-ide/ide-pipeline-spec-buffer)))
+      (progn
+        (with-current-buffer (get-buffer-create acab-ide/ide-pipeline-spec-buffer)
+          ;; insert default institution contents
+          (trie-mode)
+          (org-mode)
+          (yas-expand-snippet (yas-lookup-snippet "pipeline" 'trie-mode))
+          (write-file (f-join location acab-ide/ide-pipeline-spec-buffer))
+          )
+        )
+    )
+
+
+  (acab-ide/build-working-group-buffer)
+  (with-current-buffer "rule_stub"
+    (trie-mode)
+    (yas-expand-snippet (yas-lookup-snippet "rule" 'trie-mode) (point-min))
+    )
+
+  (with-current-buffer acab-ide/inputs-buffer-name
+    (insert "AVAILABLE INPUTS Layer 0:\n--------------------\n\n")
+    )
+  (with-current-buffer acab-ide/outputs-buffer-name
+    (insert "AVAILABLE OUTPUTS Layer 1:\n--------------------\n\n")
+    )
+  (with-current-buffer acab-ide/logging-buffer-name
+    (insert "LOGGING:\n--------------------\n\n")
+    )
+  )
+(defun acab-ide/build-working-group-buffer ()
+  (with-current-buffer acab-ide/working-group-buffer-name
+    (org-mode)
+    (insert "* Working Group\n")
+    (mapc (lambda (x) (insert "** " x ":\n")) acab-ide/working-group-buffer-headings)
+    )
+  )
+
+;;UPDATE
+(defun acab-ide/update-buffer-contents ()
+  (interactive)
+  ;;TODO update buffer contents
+  )
+(defun acab-ide/sort-conditions-and-actions ()
+  ;;TODO sort conditions and actions
+  )
+
+(defun acab-ide/show-analysis-results ()
+  ;;TODO show analysis results
   )

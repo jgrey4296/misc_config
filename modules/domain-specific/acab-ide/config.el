8e5e4d8436470ddd1f2f5b8384d0bfdf13c1c8b0
John Grey <johngrey4296@gmail.com>
2021-05-26 20:08:28 +0100

    remove helm stuff for now

--- modules/domain-specific/acab-ide/config.el
+++ modules/domain-specific/acab-ide/config.el
@@ -9,10 +9,6 @@
   ;; TODO upgrade to org-superstar?
   (add-hook 'trie-mode-hook 'org-bullets-mode)
 )
-(after! helm
-  (load! "+helms")
-  )
-
 (use-package! acab-ide
   :commands acab-ide-minor-mode
   )

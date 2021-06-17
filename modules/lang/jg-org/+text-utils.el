13959cf9fceb6da7049f750535a9ebdb20d34c11
John Grey <johngrey4296@gmail.com>
2021-06-17 21:38:19 +0100

    refine duplicate tweet remover

--- modules/lang/jg-org/+text-utils.el
+++ modules/lang/jg-org/+text-utils.el
@@ -124,7 +124,7 @@ Sort, align, split, save "
   (interactive)
   (message "Removing Duplicate Tweet Entries")
   (let ((permalinks (make-hash-table :test 'equal))
-        ;;Get all entries' (permalink start-bound end-bound level)
+        ;;Get all entries' (permalink start-bound level is_quote)
         (all-entries (seq-filter 'identity (org-map-entries '+jg-org-map-entries-build-permalink-regions)))
         (to-remove '())
         (archive-buffer (get-buffer-create "*Org-Cleanup-Tweets*"))
@@ -143,18 +143,21 @@ Sort, align, split, save "
     ;;Now remove those duplicates
     (cl-loop for tweet in to-remove
              do (let* ((begin (nth 1 tweet))
-                       (end (progn
-                              (goto-char begin)
-                              (plist-get (cadr (org-element-at-point)) :end)))
-                       )
-                  (message "Getting: %s" tweet)
+                       (elem-data (progn
+                                    (goto-char begin)
+                                    (cadr (org-element-at-point))))
+                       (end (plist-get elem-data :end))
+                       (rem-perma (plist-get elem-data :CUSTOM_ID)))
+
+                  (message "Removing: %s : %s" (nth 0 tweet) rem-perma)
+                  (assert (s-equals? (nth 0 tweet) rem-perma))
                   ;; copy tweet into deletion buffer
                   (princ (buffer-substring-no-properties begin (- end 1))
                          archive-buffer)
-                  ;; replace it in the original with a replaced link to the remaining
+                  ;; replace it in the original with a link to the remaining
                   (delete-region begin (- end 1))
                   (goto-char begin)
-                  ;; TODO Make this a link:
+                  ;; As a Link:
                   (insert (format "%s [[#%s][Duplicate of %s]]\n\n"
                                   (make-string (nth 2 tweet) ?*)
                                   (nth 0 tweet)
@@ -170,12 +173,13 @@ Sort, align, split, save "
          (permalink (plist-get entry-details :PERMALINK))
          (begin (plist-get entry-details :begin))
          (level (plist-get entry-details :level))
+         (is-quote (plist-get entry-details :IS_QUOTE))
          permalink-id
          )
     (if (and permalink begin level (string-match "\\[\\[.+?\\]\\[\\(.+?\\)\\]\\]" permalink))
         (progn (setq permalink-id (match-string 1 permalink))
                (org-set-property "CUSTOM_ID" permalink-id)
-               `(,permalink-id ,begin ,level))
+               `(,permalink-id ,begin ,level ,is-quote))
       nil)
     )
   )

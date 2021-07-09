f5f0135efa339f58345647cb75c1c4b43bc11fda
John Grey <johngrey4296@gmail.com>
2021-07-09 17:46:59 +0100

    add lisp binding for melpa

--- modules/lang/jg-lisp/+bindings.el
+++ modules/lang/jg-lisp/+bindings.el
@@ -3,5 +3,6 @@
 (map! :map emacs-lisp-mode-map
       :localleader
       :desc "Sort Defuns" "S" #'+jg-lisp-sort-defuns
-      :desc "Lisp Reference" "g 1" (cmd! (+jg-browse-url "https://www.gnu.org/software/emacs/manual/html_node/elisp/index.html"))
+      :desc "Docs: Lisp" "g 1" (cmd! (+jg-browse-url "https://www.gnu.org/software/emacs/manual/html_node/elisp/index.html"))
+      :desc "Docs: Melpa" "g 0" (cmd! (+jg-browse-url "https://melpa.org/#/"))
       )

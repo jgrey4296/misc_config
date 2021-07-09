049215d3bf9d6b28b52e96e0cf8b6951bbc06ee8
John Grey <johngrey4296@gmail.com>
2021-07-09 17:47:15 +0100

    update tag matching function

--- modules/emacs/jg-tag/+funcs.el
+++ modules/emacs/jg-tag/+funcs.el
@@ -4,9 +4,7 @@
   (interactive)
   (let* ((av-dirs (f-entries jg-tag-loc-twitter
                                    #'(lambda (x) (cond ((f-file? x) nil)
-                                                  ((s-matches? "ungrouped$" x) t)
-                                                  ((s-matches? "untagged" x) t)
-                                                  (t nil)))))
+                                                  (t t)))))
          (chosen-dir (seq-random-elt av-dirs))
          (av-orgs (f-files chosen-dir #'(lambda (x) (f-ext? x "org"))))
          (chosen-org (seq-random-elt av-orgs))

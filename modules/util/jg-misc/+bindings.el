2f9c03312027923b3ccdc0856a31527e15f13dfd
John Grey <johngrey4296@gmail.com>
2021-07-09 17:45:21 +0100

    add csharp, shell-mode and fsharp doc bindings

--- modules/util/jg-misc/+bindings.el
+++ modules/util/jg-misc/+bindings.el
@@ -21,3 +21,22 @@
         )
   (evil-make-intercept-map free-keys-mode-map)
   )
+
+(map! :map (sh-mode-map shell-mode-map)
+      :localleader
+      :desc "Docs: Brew"  "1" (cmd! (+jg-browse-url "https://brew.sh/"))
+      :desc "Docs: Awk"   "2" (cmd! (+jg-browse-url "https://www.gnu.org/software/gawk/manual/gawk.html"))
+      )
+
+(map! :map csharp-mode-map
+      :localhost
+      :desc "Docs: Unity"    "1" (cmd! (+jg-browse-url "https://docs.unity3d.com/Manual/index.html"))
+      :desc "Docs: Unity C#" "2" (cmd! (+jg-browse-url "https://docs.unity3d.com/ScriptReference/index.html"))
+      :desc "Docs: Mono"     "3" (cmd! (+jg-browse-url "http://docs.go-mono.com/"))
+      :desc "Docs: MS C#"    "4" (cmd! (+jg-browse-url "https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/"))
+)
+
+(map! :map fsharp-mode-map
+      :localhost
+      :desc "Docs: F#"       "1" (cmd! (+jg-browse-url "https://docs.microsoft.com/en-us/dotnet/fsharp/"))
+      )

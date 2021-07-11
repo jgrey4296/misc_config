b58e665f95cd83d9a95157ea2069b5a0246e4825
John Grey <johngrey4296@gmail.com>
2021-07-11 17:14:49 +0100

    DOCUMENT: add comments for top level acab-ide
    Description:

--- packages/acab-ide/acab-ide.el
+++ packages/acab-ide/acab-ide.el
@@ -1,22 +1,29 @@
 ;; -*- mode: emacs-lisp; lexical-binding: t; -*-
-
 ;; The master Acab-IDE access point
+
+;; Components
 (require 'acab-comint)
 (require 'acab-face)
 (require 'acab-hydras)
+
+;; Modes
 (require 'acab-log-mode)
 (require 'acab-inst-mode)
 (require 'acab-rule-mode)
 (require 'acab-sequence-mode)
+(require 'acab-ide-minor-mode)
 
+;; trie Specific
 (require 'trie-explore-mode)
 (require 'trie-management)
 (require 'trie-minor-mode)
 (require 'trie-mode)
 (require 'trie-data)
 (require 'trie-company)
-(require 'acab-ide-minor-mode)
 
+;; Autoloads
 (require 'acab-autoload)
 
 (provide 'acab-ide)
+
+;; TODO add a parse-checker

8a57e36fcdd186d69b7170847f25a35fda7a098b
John Grey <johngrey4296@gmail.com>
2022-01-31 16:02:21 +0000

    [packages] unpin python packages

--- packages.el
+++ packages.el
@@ -3,3 +3,11 @@
 
 (unpin! helpful dash)
 (unpin! evil-iedit-state)
+(unpin! python)
+(unpin! anaconda-mode)
+(unpin! company-anaconda)
+(unpin! pyenv-mode)
+(unpin! pyvenv)
+(unpin! python-pytest)
+(unpin! pyimport)
+(unpin! py-isort)

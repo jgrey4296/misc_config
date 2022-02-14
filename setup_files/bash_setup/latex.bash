ac9d40dea7b2dc417d2f85c7aa277e2f048c8898
John Grey <johngrey4296@gmail.com>
2022-02-14 17:21:31 +0000

    [shell] update path and cron conda

--- setup_files/bash_setup/latex.bash
+++ setup_files/bash_setup/latex.bash
@@ -2,9 +2,10 @@
 
 
 #for latex (mactex, when installed):
-PATH=/usr/local/texlive/2017/bin/x86_64-darwin:$PATH
+# PATH=/usr/local/texlive/2017/bin/x86_64-darwin:$PATH
+
 #for BasicTex:
-PATH=/usr/local/texlive/2020basic/bin/x86_64-darwin/:$PATH
+PATH=/usr/local/texlive/2021basic/bin/universal-darwin:$PATH
 
 #TEX:
 # export TEXINPUTS=/Volumes/DOCUMENTS/Dropbox/Scripts/tex/:$TEXINPUTS

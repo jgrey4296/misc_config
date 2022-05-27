e4a63286a1d397351cf1d8a70c12d0e1a35bc6e2
John Grey <johngrey4296@gmail.com>
2022-05-27 16:55:23 +0100

    [tags] update locations

--- modules/emacs/jg-tag/+vars.el
+++ modules/emacs/jg-tag/+vars.el
@@ -6,12 +6,12 @@
               jg-tag-alt-mapping (make-hash-table)
 
               jg-tag-loc-bookmarks              "~/github/writing/resources/bookmarks/total.bookmarks"
-              jg-tag-loc-global-tags            "~/github/writing/resources/cron_reports/totals.tags"
+              jg-tag-loc-global-tags            "~/github/writing/resources/cron_reports/totals_total.tags"
               jg-tag-loc-twitter-account-index  "~/github/writing/resources/cron_reports/tw_acct.index"
               jg-tag-loc-twitter-tag-index      "~/github/writing/resources/cron_reports/tw_tag.index"
               jg-tag-loc-default-helm-directory "~/github/writing/resources/"
               jg-tag-loc-twitter                "/Volumes/documents/twitterthreads/"
-              jg-tag-loc-twitter-grep-index     "/Volumes/documents/github/writing/resources/cron_reports/grep_tags.index"
+              jg-tag-loc-twitter-grep-index     "~/github/writing/resources/cron_reports/grep_tags.index"
 
               jg-tag-marker (make-marker)
               jg-tag-twitter-heading-helm-candidates nil
@@ -19,7 +19,7 @@
               jg-tag-twitter-grep-helm-candidates nil
               jg-tag-helm-buffer-name "*Helm Tags*"
 
-              jg-tag-all-loc "/Volumes/documents/github/writing/resources/cron_reports/totals.tags"
+              jg-tag-all-loc "~/github/writing/resources/cron_reports/totals_total.tags"
               )
 
 (defvar jg-tag-bookmark-helm-source)

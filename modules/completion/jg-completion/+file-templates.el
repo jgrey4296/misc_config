52040f07b4ed3bedf837e78cde6ab9796069a875
John Grey <johngrey4296@gmail.com>
2022-05-31 22:24:15 +0100

    [file-templates] update

--- modules/completion/jg-completion/+file-templates.el
+++ modules/completion/jg-completion/+file-templates.el
@@ -18,38 +18,39 @@
         ("/main\\.java$"    :trigger "__main"         :mode java-mode)
         ("/build\\.gradle$" :trigger "__build.gradle" :mode android-mode)
         ("/src/.+\\.java$" :mode java-mode)
-        ;; javascript
-        ("/package\\.json$"        :trigger "__package.json" :mode json-mode)
-        ("/bower\\.json$"          :trigger "__bower.json" :mode json-mode)
-        ("/gulpfile\\.js$"         :trigger "__gulpfile.js" :mode js-mode)
-        ("/webpack\\.config\\.js$" :trigger "__webpack.config.js" :mode js-mode)
-        ("\\.js\\(?:on\\|hintrc\\)$" :mode json-mode)
-        ;; Lua
-        ("/main\\.lua$" :trigger "__main.lua" :mode love-mode)
-        ("/conf\\.lua$" :trigger "__conf.lua" :mode love-mode)
         ;; Markdown
         (markdown-mode)
         ;; Markdown
-        (nxml-mode)
         (sh-mode)
-        ;; Solidity
-        (solidity-mode :trigger "__sol"))
       )
 
 ;; NOTE: Order from lowest - highest priority
 ;; Org
 (set-file-templates!
- '("/README\\.org$" :when +file-templates-in-emacs-dirs-p :trigger "__doom-readme" :mode org-mode)
+ '("/README\\.org$"       :trigger "__doom-readme"            :mode org-mode :when +file-templates-in-emacs-dirs-p )
+ '("contact\\.org$"       :trigger "__contact"                :mode org-mode)
+ '("invoice\\.org$"       :trigger "__invoice"                :mode org-mode)
+ '("project\\.org$"       :trigger "__project"                :mode org-mode)
+ '("design_doc\\.org$"    :trigger "__designDocNotes"         :mode org-mode)
+ '("inst_pipeline\\.org$" :trigger "__institution_pipeline"   :mode org-mode)
+ '("lit_review\\.org$"    :trigger "__lit_review"             :mode org-mode)
+ '("two_pager\\.org$"     :trigger "__pacheco_vega_two_pager" :mode org-mode)
+
  '(org-journal-mode :ignore t)
  '(org-mode)
  )
 
 ;; Python
 (set-file-templates!
- '(python-mode     :trigger "__")
- '("test_.+\\.py$" :trigger "__tests" :mode python-mode)
- '("_.+\\.py$"     :trigger "__"      :mode python-mode)
- '("\\.py$"        :trigger "__"      :mode python-mode)
+ '(python-mode      :trigger "__")
+ '("setup\\.py"     :trigger "__setup"          :mode python-mode)
+ '("setup\\.cfg"    :trigger "__setup_cfg"      :mode python-mode)
+ '("conf\\.py"      :trigger "__conf"           :mode python-mode)
+ '("pyproject.toml" :trigger "__pyproject_toml" :mode python-mode)
+ '("LICENSE"        :trigger "__license-acab" :mode text-mode)
+ '("test_.+\\.py$"  :trigger "__tests" :mode python-mode)
+ '("_.+\\.py$"      :trigger "__"      :mode python-mode)
+ '("\\.py$"         :trigger "__"      :mode python-mode)
  )
 
 

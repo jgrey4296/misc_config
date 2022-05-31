3c1ed58380cbe6ec9f5071a12324a701e23d9ea6
John Grey <johngrey4296@gmail.com>
2022-05-31 21:16:19 +0100

    [file-templates] update

--- modules/completion/jg-completion/+file-templates.el
+++ modules/completion/jg-completion/+file-templates.el
@@ -2,6 +2,8 @@
 
 (message "Setting file templates dir")
 (setq +file-templates-dir "/Volumes/documents/github/emacs_files/snippets/file-templates")
+(setq yas-snippet-dirs '(+snippets-dir doom-snippets-dir +file-templates-dir yasnippet-snippets-dir))
+(setq yas--default-user-snippets-dir yas-snippet-dirs)
 
 (setq +file-templates-alist
       '(;; General
@@ -13,7 +15,7 @@
         ;; direnv
         ("/\\.envrc$" :trigger "__envrc" :mode direnv-envrc-mode)
         ;; java
-        ("/main\\.java$" :trigger "__main" :mode java-mode)
+        ("/main\\.java$"    :trigger "__main"         :mode java-mode)
         ("/build\\.gradle$" :trigger "__build.gradle" :mode android-mode)
         ("/src/.+\\.java$" :mode java-mode)
         ;; javascript
@@ -34,31 +36,31 @@
         (solidity-mode :trigger "__sol"))
       )
 
+;; NOTE: Order from lowest - highest priority
 ;; Org
-(set-file-templates! '("/README\\.org$" :when +file-templates-in-emacs-dirs-p :trigger "__doom-readme" :mode org-mode)
-                     '(org-journal-mode :ignore t)
-                     '(org-mode))
+(set-file-templates!
+ '("/README\\.org$" :when +file-templates-in-emacs-dirs-p :trigger "__doom-readme" :mode org-mode)
+ '(org-journal-mode :ignore t)
+ '(org-mode)
+ )
 
 ;; Python
-(set-file-templates! '(python-mode :trigger "__")
-                     '("\\.py$" :trigger "__" :mode python-mode)
-                     '("_.+\\.py$" :trigger "__" :mode python-mode)
-                     '("test_.+\\.py$" :trigger "__tests" :mode python-mode))
+(set-file-templates!
+ '(python-mode     :trigger "__")
+ '("test_.+\\.py$" :trigger "__tests" :mode python-mode)
+ '("_.+\\.py$"     :trigger "__"      :mode python-mode)
+ '("\\.py$"        :trigger "__"      :mode python-mode)
+ )
 
 
 ;; Lisp
-(set-file-templates! '("/.dir-locals.el$")
-                     '("/packages\\.el$" :when +file-templates-in-emacs-dirs-p
-                       :trigger "__doom-packages"
-                       :mode emacs-lisp-mode)
-                     '("/doctor\\.el$" :when +file-templates-in-emacs-dirs-p
-                       :trigger "__doom-doctor"
-                       :mode emacs-lisp-mode)
-                     '("/test/.+\\.el$" :when +file-templates-in-emacs-dirs-p
-                       :trigger "__doom-test"
-                       :mode emacs-lisp-mode)
-                     '("\\.el$" :when +file-templates-in-emacs-dirs-p
-                       :trigger "__doom-module"
-                       :mode emacs-lisp-mode)
-                     '(emacs-lisp-mode :trigger "__package")
-                     '("-test\\.el$" :mode emacs-ert-mode))
+(set-file-templates!
+ '("/.dir-locals.el$")
+ '("-test\\.el$" :mode emacs-ert-mode)
+ '(emacs-lisp-mode :trigger "__package")
+ '("\\.el$" :when +file-templates-in-emacs-dirs-p :trigger "__doom-module" :mode emacs-lisp-mode)
+ '("packages\\.el$" :when +file-templates-in-emacs-dirs-p :trigger "__doom_packages" :mode emacs-lisp-mode)
+ '("config\\.el$"   :when +file-templates-in-emacs-dirs-p :trigger "__doom_config"   :mode emacs-lisp-mode)
+ '("/doctor\\.el$"  :when +file-templates-in-emacs-dirs-p :trigger "__doom-doctor"   :mode emacs-lisp-mode)
+ '("/test/.+\\.el$" :when +file-templates-in-emacs-dirs-p :trigger "__doom-test"     :mode emacs-lisp-mode)
+ )

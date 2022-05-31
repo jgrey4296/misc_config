5d10fcdef8f96b2d8277e241d973004e77fcb83b
John Grey <johngrey4296@gmail.com>
2022-05-31 22:24:31 +0100

    [bash] update aliases

--- setup_files/bash_setup/misc.bash
+++ setup_files/bash_setup/misc.bash
@@ -1,18 +1,22 @@
 #!/usr/bin/env bash
 
+alias cpu="top -l1 -n0"
 alias browse="python -mwebbrowser"
-
 alias lsa="ls -lha"
 alias lsd="gls -la --group-directories-first"
 alias lsda="ls -la | grep '^d'"
-
 alias ql="qlmanage -p 2>/dev/null"
-
 alias cd="mycd $1"
 alias cdc="mycdc $1"
 alias cda="mycda $1"
-
 alias pse="ps -A | grep emacs"
+alias jgf="find . -maxdepth 1 -iname $1"
+#disk usage:
+alias duh="du -hd 1 | sort"
+#Disable osx gatekeeper: (needs sudo)
+alias gatekeeper="spctl --master-disable"
+# pdf creator
+alias txt2pdf="cupsfilter"
 
 function mycd(){
     builtin cd "$@" && ls
@@ -26,15 +30,6 @@ function mycda(){
     builtin cd "$@" && lsa
 }
 
-alias jgf="find . -maxdepth 1 -iname $1"
-
-#disk usage:
-alias jgdu="du -hd 1 | sort"
-#Disable osx gatekeeper: (needs sudo)
-alias gatekeeper="spctl --master-disable"
-
-# pdf creator
-alias txt2pdf="cupsfilter"
 
 #setting up the prompt:
 PS1='(\j): \D{%D} \A \u:  '

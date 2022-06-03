c203b69c0191ed0f5e991f5da2584428f34a95ec
John Grey <johngrey4296@gmail.com>
2022-06-03 22:02:54 +0100

    [chore]

--- setup_files/cron/dropbox_watcher.sh
+++ setup_files/cron/dropbox_watcher.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+source $HOME/.doom.d/setup_files/bash_setup/python.bash
 conda_activate_for_scripts bookmark
 
 WATCH=( "$HOME/Dropbox/docs" "$HOME/Downloads" )
@@ -14,6 +15,10 @@ do
     mv -u -n *.pdf $TARGET 2> /dev/null || echo "No pdfs to move"
     mv -u -n *.epub $TARGET 2> /dev/null || echo "No Epubs to move"
 
+    find . -maxdepth 1 \( -name "*.pdf" -o -name "*.epub" \) -printf "%f\n" | xargs -I {} mv {} exists_{}
+
 done
 
+echo "Stubbing"
 bkmk-bot-stub
+echo "Stubbing finished"

283b68b66a01d78c8c72ccf532c6773e9e3eef2f
John Grey <johngrey4296@gmail.com>
2022-09-22 04:46:14 +0100

    [chore]

--- setup_files/bash_setup/ssh.bash
+++ setup_files/bash_setup/ssh.bash
@@ -1,6 +1,3 @@
 #!/usr/bin/env bash
 
-#ssh
-alias mini="ssh jg@192.168.1.5"
-alias ubun="ssh john@192.168.1.11"
-alias wind="ssh john@192.168.1.2"
+# remember to setup /etc/ssh/sshd_config to use public keys

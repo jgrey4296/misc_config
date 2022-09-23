e881ac252d3e72d0ced390958eabf139687953bc
John Grey <johngrey4296@gmail.com>
2022-09-23 15:35:26 +0100

    [text] add chapter-split function

---
+++ modules/util/text-manipulation/+chapter-split.el
@@ -0,0 +1,51 @@
+(defun +jg-text-split-files-into-chapters ()
+  " Split a group of marked files into separate chapter html files,
+using a list of chapter titles"
+  (interactive)
+  (let* ((files (dired-get-marked-files))
+         (title-file (read-file-name "Chapter File: " default-directory))
+         (titles (with-file-contents! title-file
+                   (split-string (buffer-string) "\n" t)))
+         (current 1)
+         start end substring
+         )
+    (message "Titles: %s" titles)
+    (pop titles)
+    (cl-loop for file in files
+             do
+             (with-file-contents! file
+               (re-search-forward "<body class=\"calibre\">$" nil t)
+               (forward-line) (beginning-of-line)
+               (setq start (point))
+               (while (and titles (search-forward (car titles) nil 1))
+                 (message "Searched for: %s" (car titles))
+                 (cond ((not start)
+                        (message "Setting start")
+                        (pop titles)
+                        (cl-incf current)
+                        (beginning-of-line)
+                        (setq start (point))
+                        )
+                       (start
+                        (beginning-of-line)
+                        (setq end (point))
+                        (message "Appending: %s" (s-trim (buffer-substring start end)))
+                        (append-to-file start end (format "ch%s.html" current))
+                        (append-to-file "\n" nil (format "ch%s.html" current))
+                        (forward-line -1)
+                        (setq start (if titles nil (point))
+                              end nil)
+                        ))
+                 )
+               (when start
+                 (setq end (point-max))
+                 (message "Final : %s" (s-trim (buffer-substring start end)))
+                 (append-to-file start end (format "ch%s.html" current))
+                 (append-to-file "\n" nil (format "ch%s.html" current))
+                 (setq start nil
+                       end nil)
+                 )
+               )
+             )
+    )
+  )

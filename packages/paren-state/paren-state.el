082aa0aec8daab9a85e3199898cdb77f91e51aff
John Grey <johngrey4296@gmail.com>
2022-11-10 08:50:53 +0000

    [paren-state] adjust

--- packages/paren-state/paren-state.el
+++ packages/paren-state/paren-state.el
@@ -10,18 +10,25 @@
     ;; :enable (motion)
     ;; :input-method t
     ;; :suppress-keymap t
+    (setq-local sp-navigate-reindent-after-up nil
+                sp-navigate-reindent-after-up-in-string nil
+                )
     )
 ;; (evil-add-hjkl-bindings global-map 'paren)
 (suppress-keymap evil-paren-state-map)
-(evil-global-set-key 'paren "h" #'sp-beginning-of-sexp)
-(evil-global-set-key 'paren "l" #'sp-end-of-sexp)
-(evil-global-set-key 'paren "k" #'sp-up-sexp)
+(evil-global-set-key 'paren "H" #'sp-beginning-of-sexp)
+(evil-global-set-key 'paren "h" #'sp-backward-sexp)
+(evil-global-set-key 'paren "l" #'sp-forward-sexp)
+(evil-global-set-key 'paren "L" #'sp-end-of-sexp)
+(evil-global-set-key 'paren "k" #'sp-backward-up-sexp)
+(evil-global-set-key 'paren "K" #'sp-up-sexp)
 (evil-global-set-key 'paren "j" #'sp-down-sexp)
 
 (evil-global-set-key 'paren "p" #'sp-beginning-of-next-sexp)
 (evil-global-set-key 'paren "u" #'sp-beginning-of-previous-sexp)
 (evil-global-set-key 'paren "o" #'sp-end-of-next-sexp)
 (evil-global-set-key 'paren "i" #'sp-end-of-previous-sexp)
+(evil-global-set-key 'paren "q" #'evil-force-normal-state)
 
 ;; (evil-global-set-key 'paren "a" #'sp-previous-sexp)
 ;; (evil-global-set-key 'paren "f" #'sp-next-sexp)

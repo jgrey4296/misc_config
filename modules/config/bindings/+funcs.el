6f9cf74f221e963a9dba081c5973222fa9ec06d9
John Grey <johngrey4296@gmail.com>
2023-03-28 14:32:57 +0100

    [binding] add calendar insert date
    --------------------------------------------------
    Warnings:
    
    --------------------------------------------------
    Basic Explanation:
    
    --------------------------------------------------
    Outstanding issues:
    
    --------------------------------------------------
    Notes:

--- modules/config/bindings/+funcs.el
+++ modules/config/bindings/+funcs.el
@@ -73,3 +73,13 @@
   (interactive)
   (bookmark-load (read-file-name "Load bookmarks file: " doom-user-dir "bookmarks" t "bookmarks") t)
   )
+
+(defun +jg-bindings-calendar-insert-date ()
+  " from https://emacs.stackexchange.com/questions/42529/insert-date-using-a-calendar "
+  (interactive)
+   "Capture the date at point, exit the Calendar, insert the date."
+   (interactive)
+   (seq-let (month day year) (save-match-data (calendar-cursor-to-date))
+     (calendar-exit)
+     (insert (format "%d-%02d-%02d" year month day)))
+  )
--- modules/config/bindings/+misc-bindings.el
+++ modules/config/bindings/+misc-bindings.el
@@ -122,6 +122,10 @@
 (define-key evil-motion-state-map [mouse-5] #'ignore)
 ;;-- end Mouse Deactivation
 
+(map! :map calendar-mode-map
+      :n "RET" #'+jg-bindings-calendar-insert-date
+      )
+
 (after! which-key
   (let ((prefix-re (regexp-opt (list doom-leader-key doom-leader-alt-key))))
     (cl-pushnew `((,(format "\\`\\(?:C-w\\|%s w\\) m\\'" prefix-re))

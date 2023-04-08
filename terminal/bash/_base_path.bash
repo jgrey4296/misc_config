11ee2e640d0f0e951112a3f5dabacec9d1d4f2c6
John Grey <johngrey4296@gmail.com>
2023-04-08 17:39:08 +0100

    [bash] adjust path
    --------------------------------------------------
    Warnings:
    
    --------------------------------------------------
    Basic Explanation:
    
    --------------------------------------------------
    Outstanding issues:
    
    --------------------------------------------------
    Notes:

--- terminal/bash/_base_path.bash
+++ terminal/bash/_base_path.bash
@@ -1,8 +1,9 @@
 jgdebug Setting Initial Path
 # PATHS:
-PATH=/sbin:/usr/sbin:/bin:/usr/bin
-PATH=/usr/local/sbin:$PATH
-PATH=/usr/local/bin:$PATH
+PATH=/usr/bin:/usr/sbin:/bin:/sbin        # Core
+PATH=/usr/bin:/usr/sbin:$PATH             # Secondary
+PATH=/usr/local/bin:/usr/local/sbin:$PATH # Tertiary
+
 PATH=/usr/local/opt/coreutils/libexec/gnubin:$PATH
 PATH=/usr/local/opt/make/libexec/gnubin:$PATH
 PATH=/usr/local/opt/findutils/libexec/gnubin:$PATH
@@ -14,10 +15,10 @@ jgdebug Setting calibre
 PATH=/Applications/calibre.app/Contents/MacOS:$PATH
 
 jgdebug Setting blender
-PATH=/Applications/blender/blender.app/Contents/MacOS:$PATH
+PATH=/Applications/Blender.app/Contents/MacOS:$PATH
 
 # MAN Paths
 # https://www.howtogeek.com/682871/how-to-create-a-man-page-on-linux/
-MANPATH=/usr/share/man:/usr/local/share/man:/usr/local/man
+MANPATH=/usr/local/man:/usr/local/share/man:/usr/share/man
 MANPATH="$HOME/.doom.d/terminal/man/main":$MANPATH
 # MANPATH=/Library/Apple/usr/share/man:$MANPATH

70cab3ae0b25cc5e699fa6ca10830a24b7e93c20
John Grey <johngrey4296@gmail.com>
2023-04-22 13:53:54 +0100

    [modules] update modules
    --------------------------------------------------
    Warnings:
    
    --------------------------------------------------
    Basic Explanation:
    
    --------------------------------------------------
    Outstanding issues:
    
    --------------------------------------------------
    Notes:

--- init.el
+++ init.el
@@ -7,13 +7,13 @@
 
  :editor char-insert fold navigation projects tagging text-manipulation
  :editor (evil +everywhere) format
- :editor fold snippets word-wrap rotate-text lookup
+ :editor fold snippets word-wrap lookup
  ;;:editor multiple-cursors
 
  :emacs electric vc
  :emacs dired hydra ibuffer misc popup version-control
 
- ;; :lang-data csv graphql
+ :lang-data csv graphql
  :lang-data xml toml json yaml logs
 
  ;; :lang-dsl cobol fortran solidity
@@ -22,24 +22,21 @@
  :lang-dsl qt
  ;; :lang-dsl music faust
 
- ;; :lang-strongly-typed fstar haskell
- ;; :lang-strongly-typed idris lean ocaml scala sml
+ :lang-strongly-typed fstar haskell
+ :lang-strongly-typed idris lean
  :lang-strongly-typed coq rust
- :lang-strongly-typed jvm-langs dotnet-langs
+ :lang-strongly-typed jvm-langs dotnet-langs ml-langs
 
  :lang-text inform web
  :lang-text (org +gnuplot +pomodoro +journal +noter)
  :lang-text bibtex latex markdown plantuml rst
 
  :lang-weakly-typed lisp-langs (python +conda +pyright +cython +lsp) lua
- ;; :lang-weakly-typed erlang-vms godot ruby
+ :lang-weakly-typed erlang-vms godot ruby
 
  :lang emacs-lisp ledger
- ;; :lang agda csharp elixir erlang ess
- ;; :lang fsharp gdscript
- ;; :lang julia kotlin
- ;; :lang racket rst
- ;; :lang scheme
+ ;; :lang agda ess
+ ;; :lang julia
 
  :ui deft doom
  :ui hl-todo indent-guides ligatures

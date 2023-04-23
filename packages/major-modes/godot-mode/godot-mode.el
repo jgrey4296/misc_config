047054165632e754751375255ba71fefbcf905b7
John Grey <johngrey4296@gmail.com>
2023-04-23 17:00:13 +0100

    [stub] add godot mode stub
    --------------------------------------------------
    Warnings:
    
    --------------------------------------------------
    Basic Explanation:
    
    --------------------------------------------------
    Outstanding issues:
    
    --------------------------------------------------
    Notes:

---
+++ packages/major-modes/godot-mode/godot-mode.el
@@ -0,0 +1,33 @@
+;;; godot-mode.el -*- lexical-binding: t; no-byte-compile: t; -*-
+;;-- header
+;;
+;; Copyright (C) 2023 John Grey
+;;
+;; Author: John Grey <https://github.com/jgrey4296>
+;; Maintainer: John Grey <johngrey4296 at gmail.com>
+;; Created: April 23, 2023
+;; Modified: April 23, 2023
+;; Version: 0.0.1
+;; Keywords:
+;; Homepage: https://github.com/jgrey4296
+;; Package-Requires: ((emacs "24.3"))
+;; Package written on: ((emacs 28.2))
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; Commentary:
+;;
+;;
+;;
+;;; Code:
+
+;;-- end header
+
+;;-- imports
+(load 'tres-mode)
+
+;;-- end imports
+
+
+(provide 'godot-mode)
+;;; godot-mode.el ends here

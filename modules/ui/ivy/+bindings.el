d19536254a5643202f232f8244194ea823609371
John Grey <johngrey4296@gmail.com>
2023-11-20 20:22:13 +0000

    [chore]:

--- modules/ui/ivy/+bindings.el
+++ modules/ui/ivy/+bindings.el
@@ -8,38 +8,39 @@
 
 ;;-- remaps
 (map! :after counsel
-      [remap bookmark-jump]              #'+jg-ivy-bookmark
-      [remap apropos]                    #'counsel-apropos
-      [remap bookmark-jump]              #'counsel-bookmark
-      [remap compile]                    #'+ivy/compile
-      [remap describe-bindings]          #'counsel-descbinds
-      [remap describe-face]              #'counsel-faces
-      [remap describe-function]          #'counsel-describe-function
-      [remap describe-variable]          #'counsel-describe-variable
-      [remap describe-symbol]            #'counsel-describe-symbol
-      [remap evil-show-registers]        #'counsel-evil-registers
-      [remap evil-show-marks]            #'counsel-mark-ring
-      [remap execute-extended-command]   #'counsel-M-x
-      [remap find-file]                  #'counsel-find-file
-      [remap find-library]               #'counsel-find-library
-      [remap imenu]                      #'counsel-imenu
-      [remap info-lookup-symbol]         #'counsel-info-lookup-symbol
-      [remap load-theme]                 #'counsel-load-theme
-      [remap locate]                     #'counsel-locate
-      [remap org-goto]                   #'counsel-org-goto
-      [remap org-set-tags-command]       #'counsel-org-tag
-      [remap projectile-compile-project] #'+ivy/project-compile
-      [remap recentf-open-files]         #'counsel-recentf
-      [remap set-variable]               #'counsel-set-variable
-      [remap swiper]                     #'counsel-grep-or-swiper
-      [remap insert-char]                #'counsel-unicode-char
-      [remap yank-pop]                   #'counsel-yank-pop
-      [remap projectile-find-file]        #'+ivy/projectile-find-file
-      [remap projectile-find-dir]         #'counsel-projectile-find-dir
-      [remap projectile-switch-to-buffer] #'counsel-projectile-switch-to-buffer
-      [remap projectile-grep]             #'counsel-projectile-grep
-      [remap projectile-ag]               #'counsel-projectile-ag
-      [remap projectile-switch-project]   #'counsel-projectile-switch-project
+      [remap bookmark-jump]                 #'+jg-ivy-bookmark
+      [remap apropos]                       #'counsel-apropos
+      ;; [remap bookmark-jump]              #'counsel-bookmark
+      [remap compile]                       #'+ivy/compile
+      [remap projectile-compile-project]    #'+ivy/project-compile
+      [remap projectile-find-file]          #'+ivy/projectile-find-file
+
+      [remap describe-bindings]             #'counsel-descbinds
+      [remap describe-face]                 #'counsel-faces
+      [remap describe-function]             #'counsel-describe-function
+      [remap describe-variable]             #'counsel-describe-variable
+      [remap describe-symbol]               #'counsel-describe-symbol
+      [remap evil-show-registers]           #'counsel-evil-registers
+      [remap evil-show-marks]               #'counsel-mark-ring
+      [remap execute-extended-command]      #'counsel-M-x
+      [remap find-file]                     #'counsel-find-file
+      [remap find-library]                  #'counsel-find-library
+      [remap imenu]                         #'counsel-imenu
+      [remap info-lookup-symbol]            #'counsel-info-lookup-symbol
+      [remap load-theme]                    #'counsel-load-theme
+      [remap locate]                        #'counsel-locate
+      [remap org-goto]                      #'counsel-org-goto
+      [remap org-set-tags-command]          #'counsel-org-tag
+      [remap recentf-open-files]            #'counsel-recentf
+      [remap set-variable]                  #'counsel-set-variable
+      [remap swiper]                        #'counsel-grep-or-swiper
+      [remap insert-char]                   #'counsel-unicode-char
+      [remap yank-pop]                      #'counsel-yank-pop
+      [remap projectile-find-dir]           #'counsel-projectile-find-dir
+      [remap projectile-switch-to-buffer]   #'counsel-projectile-switch-to-buffer
+      [remap projectile-grep]               #'counsel-projectile-grep
+      [remap projectile-ag]                 #'counsel-projectile-ag
+      [remap projectile-switch-project]     #'counsel-projectile-switch-project
       )
 ;;-- end remaps
 

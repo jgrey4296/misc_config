87a183b7858921b61b8f36ce22c52eff680a5571
John Grey <johngrey4296@gmail.com>
2024-04-17 18:19:33 +0100

    [fix]: typo

--- modules/lang-weakly-typed/python/autoload/workspace.el
+++ modules/lang-weakly-typed/python/autoload/workspace.el
@@ -4,7 +4,7 @@
 
 ;;;###autoload (autoload 'related-files:jg-python-project "lang-weakly-typed/python/autoload/workspace.el" nil t)
 (make-related! jg-python-project
-               :files ((:impl (f-join root fparent2 (s-replace "test_" "" fname)) :when is-test)
+               :files ((:impl (f-join root fparent (s-replace "test_" "" fname)) :when is-test)
                        (:test (f-join fparent "__tests" (concat "test_" fname))   :when (not is-test))
 
                        (:initpy (f-join fparent "__init__.py"))
@@ -14,8 +14,7 @@
                        ;; TODO (:docs)
                        (:log log-file                    :when (f-exists? log-file))
                        )
-               :tests ((is-test (s-matches? "^test_" fname))
-                       (is-dooter    (s-matches? "dooter.py" fname))
+               :tests ((is-test      (s-matches? "^test_" fname))
                        (is-doot-toml (s-matches? "doot.toml" fname))
                        )
                :binds ((src (f-filename root))

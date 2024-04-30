dc167de0c7b5a375770053bbb421e6df1b2f78ff
John Grey <johngrey4296@gmail.com>
2024-04-30 23:40:51 +0100

    [add]: window toggle dedicated

--- modules/editor/window-nav/+bindings.el
+++ modules/editor/window-nav/+bindings.el
@@ -22,9 +22,10 @@
 (map! :leader
       (:prefix "w"
        :desc "3 Column Centered" "3" #'+jg-windows-3-col-centered
+       :desc "Reserve" "r"           #'+jg-windows-toggle-dedicated
        )
       (:prefix "o s"
        :desc "Pop up scratch buffer"        "s"   #'+jg-window-nav--system-scratch
        :desc "open project scratch buffer"  "p"   #'+jg-window-nav--project-scratch
        )
-      )
+)

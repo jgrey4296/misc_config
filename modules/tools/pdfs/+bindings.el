689685aab686f5c121f27fdd0143cce5f9822bc8
John Grey <johngrey4296@gmail.com>
2024-08-09 20:45:39 +0100

    [add]: pdf dired commands

--- modules/tools/pdfs/+bindings.el
+++ modules/tools/pdfs/+bindings.el
@@ -1,14 +1,17 @@
 ;;; +bindings.el -*- lexical-binding: t; -*-
 
 (map! :map jg-dired-mode-map
+      :after jg-dired-bindings
       :prefix ("> p" . "Pdf calls")
-      :desc "Pdf Info"    "i" #'pdf-meta-extract-info
-      :desc "Pdf Split"   "s" #'pdf-meta-split
-      :desc "Pdf Join"    "j" #'pdf-meta-join
-      :desc "Pdf Attach"  "a" #'pdf-meta-attach
-      :desc "Pdf Unpack"  "u" #'pdf-meta-detach
-      :desc "PdfToText"   "t" #'+jg-pdf-to-text
-      :desc "Linearize"   "l" #'+jg-pdf-linearize
+      :desc "Pdf Info"        "i" #'pdf-meta-extract-info
+      :desc "Pdf Split"       "s" #'pdf-meta-split
+      :desc "Pdf Join"        "j" #'pdf-meta-join
+      :desc "Pdf Attach"      "a" #'pdf-meta-attach
+      :desc "Pdf Unpack"      "u" #'pdf-meta-detach
+      :desc "Pdf ToText"      "t" #'+jg-pdf-to-text
+      :desc "Pdf Linearize"   "l" #'+jg-pdf-linearize
+      :desc "Pdf Validate"    "v" #'+jg-pdf-validate
+      :desc "Pdf metadata"    "m" #'+jg-pdf-metadata
       )
 
 (map! :map pdf-meta-mode-map

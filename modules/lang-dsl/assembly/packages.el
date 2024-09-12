ee64fed710761c2f253e844c40e8dbbd08cce58d
John Grey <johngrey4296@gmail.com>
2024-09-12 17:38:40 +0100

    [stub]: assembly module

---
+++ modules/lang-dsl/assembly/packages.el
@@ -0,0 +1,29 @@
+;;; packages.el -*- lexical-binding: t; no-byte-compile: t; -*-
+;;-- Header
+;; File Commentary:
+;;
+;;
+;; See footer for licenses/metadata/notes as applicable
+;;-- end Header
+
+
+(package! llvm-ts-mode)
+
+
+;;-- Footer
+;; Copyright (C) 2024 john
+;;
+;; Author:     john <https://github.com/jgrey4296>
+;; Maintainer: john <john@john-UM700>
+;; Created:    September 12, 2024
+;; Modified:   September 12, 2024
+;; Version: 0.0.1
+;; Keywords:
+;; Homepage: https://github.com/jgrey4296
+;; Package-Requires: ((emacs "24.3"))
+;; Package written on: ((emacs 29.1))
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;-- end Footer
+;;; packages.el ends here

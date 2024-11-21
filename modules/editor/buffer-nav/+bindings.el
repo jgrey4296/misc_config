28b16e82f4b94bbff517b5ba65abff438b2235f1
John Grey <johngrey4296@gmail.com>
2024-11-21 19:20:36 +0000

    [add]: suspend/restore mode bindings

--- modules/editor/buffer-nav/+bindings.el
+++ modules/editor/buffer-nav/+bindings.el
@@ -1,5 +1,10 @@
 ;; +bindings.el<2> -*- lexical-binding: t; -*-
 
+(map! :leader
+      :prefix "b"
+      :desc "Suspend Mode"   ";" (cmd! (major-mode-suspend))
+      :desc "Restore Mopde"  "'" (cmd! (major-mode-restore))
+      )
 
 (map! :map jg-binding-jump-map
       :desc "Paren State" "p"  #'evil-parenM-state

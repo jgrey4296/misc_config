66000696a588167df2bcc01819dcbe83455cc431
John Grey <johngrey4296@gmail.com>
2025-01-11 17:27:45 +0000

    [incremental]: describe buffer func

--- modules/config/default/autoload/auto-modes.el
+++ modules/config/default/autoload/auto-modes.el
@@ -1,11 +1,30 @@
 ;;; auto-modes.el -*- lexical-binding: t; -*-
 
 ;;;###autoload
-(defun +jg-default-debug-auto-mode ()
+(defun +jg-default-debug-buffer-state ()
   (interactive)
   (let* ((name (file-name-sans-versions (or (buffer-file-name) (buffer-name))))
          (result (assoc-default name auto-mode-alist 'string-match))
         )
-    (message "Buffer %s (%s) Matches: %s" (buffer-name) major-mode result)
+    (message (string-join
+              (list
+               (format"Buffer %s (%s)" (buffer-name) major-mode)
+               (format "Auto-Mode: %s" result)
+               (format "(Dedicated %s) (popup %s) (Live %s) (Real %s)"
+                       (---truthy? (window-dedicated-p))
+                       (---truthy? (+popup-buffer-p))
+                       (buffer-live-p (current-buffer))
+                       (---truthy? (doom-real-buffer-p (current-buffer)))
+                       )
+               (format "(Modified %s) (in-project %s)"
+                       (---truthy? (buffer-modified-p (current-buffer)))
+                       (and (projectile-project-root)
+                            (f-ancestor-of? (projectile-project-root)
+                                            (buffer-file-name)))
+                       )
+               )
+              "\n"
+              )
+             )
     )
   )

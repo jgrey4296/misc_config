4d913e513bc25bf7af3c31bc007c87281ec32d01
John Grey <johngrey4296@gmail.com>
2025-03-22 04:16:04 +0000

    [chore]:

--- modules/ui/ibuffer/+specs.el
+++ modules/ui/ibuffer/+specs.el
@@ -1,5 +1,26 @@
 ;;; +filters.el -*- lexical-binding: t; -*-
 
+(speckler-new! ibuffer-filters (key val)
+  "Register ibuffer filters"
+  :target ibuffer-saved-filters
+  :loop 'collect
+  (cons (symbol-name key) val)
+  )
+
+(speckler-new! ibuffer-groups (key val)
+  "Register ibuffer groups"
+  :target ibuffer-saved-filter-groups
+  :loop 'collect
+  (cons (symbol-name key) val)
+  )
+
+(speckler-new! ibuffer-formats (key val)
+  "Register ibuffer formats"
+  :target ibuffer-formats
+  :loop 'collect
+  val
+  )
+
 (speckler-add! ibuffer-formats ()
   '(normal
     mark modified read-only locked
--- modules/ui/ibuffer/config.el
+++ modules/ui/ibuffer/config.el
@@ -27,24 +27,3 @@
 (use-package! ibuffer-vc
   :after ibuffer
   )
-
-(speckler-new! ibuffer-filters (key val)
-  "Register ibuffer filters"
-  :target ibuffer-saved-filters
-  :loop 'collect
-  (cons (symbol-name key) val)
-  )
-
-(speckler-new! ibuffer-groups (key val)
-  "Register ibuffer groups"
-  :target ibuffer-saved-filter-groups
-  :loop 'collect
-  (cons (symbol-name key) val)
-  )
-
-(speckler-new! ibuffer-formats (key val)
-  "Register ibuffer formats"
-  :target ibuffer-formats
-  :loop 'collect
-  val
-  )
